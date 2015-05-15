5f439381446ea782947bd4effcbd68ee2bde894e
Steve Smith <steve@alwaysagileconsulting.com>
2015-05-15 15:56:59 +0100

    MDTP-32 Added credentials GUID

--- src/main/groovy/uk/gov/hmrc/jenkinsjobs/domain/builder/JobBuilders.groovy
+++ src/main/groovy/uk/gov/hmrc/jenkinsjobs/domain/builder/JobBuilders.groovy
@@ -10,6 +10,6 @@ final class JobBuilders {
     private JobBuilders() {}
 
     static JobBuilder jobBuilders(String name1, String repository) {
-        new JobBuilder(name1, "${name1} auto-configured job", 14, 10, gitHubComScm(repository, 'jenkins'))
+        new JobBuilder(name1, "${name1} auto-configured job", 14, 10, gitHubComScm(repository, 'ce814d36-5570-4f1f-ad70-0a8333122be6'))
     }
 }
