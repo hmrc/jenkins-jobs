5f6e3d362695886344e69a09acc39b1d0a54c367
Steve Smith <steve@alwaysagileconsulting.com>
2015-05-15 12:26:08 +0100

    MDTP-32 Upgrade jenkins-job-builders to supply jenkins credentials for github

--- src/main/groovy/uk/gov/hmrc/jenkinsjobs/domain/builder/JobBuilders.groovy
+++ src/main/groovy/uk/gov/hmrc/jenkinsjobs/domain/builder/JobBuilders.groovy
@@ -10,6 +10,6 @@ final class JobBuilders {
     private JobBuilders() {}
 
     static JobBuilder jobBuilders(String name1, String repository) {
-        new JobBuilder(name1, "${name1} auto-configured job", 14, 10, gitHubComScm(repository))
+        new JobBuilder(name1, "${name1} auto-configured job", 14, 10, gitHubComScm(repository, 'jenkins'))
     }
 }
