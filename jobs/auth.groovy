662cc8474bdbfbb32025397ef1b52ab41be0fd07
Duncan Crawford <duncan@noughtsandones.co.uk>
2015-09-14 10:31:45 +0100

    http-caching-client to use java 8

--- jobs/auth.groovy
+++ jobs/auth.groovy
@@ -3,7 +3,7 @@ import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtLibraryJobBuilder
 
 import static uk.gov.hmrc.jenkinsjobbuilders.domain.variables.JdkEnvironmentVariable.JDK7
 
-new SbtLibraryJobBuilder('http-caching-client', JDK7).
+new SbtLibraryJobBuilder('http-caching-client').
         build(this as DslFactory)
 
 new SbtLibraryJobBuilder('json-encryption').
