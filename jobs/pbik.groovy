a3dbe19e92e23aa08233ec00572fef83f8ed6bc0
Adeel Ahmed <adeel.a.ahmed@accenture.com>
2015-12-21 11:40:46 +0000

    Added job for PBIK

--- jobs/pbik.groovy
+++ jobs/pbik.groovy
@@ -1,7 +1,13 @@
 import javaposse.jobdsl.dsl.DslFactory
 import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.BuildMonitorViewBuilder
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtFrontendJobBuilder
+import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtMicroserviceJobBuilder
 
 new SbtFrontendJobBuilder('pbik-frontend').
                          build(this as DslFactory)
 
+new SbtMicroserviceJobBuilder('pbik').
+                         build(this as DslFactory)
+
+new BuildMonitorViewBuilder('PBIK-MONITOR')
+        .withJobs('pbik-frontend', 'pbik').build(this)
