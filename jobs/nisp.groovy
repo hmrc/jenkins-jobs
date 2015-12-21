f0099b54dbbe2bb879efb3da2664d2dd39eb7ec8
Andrew Gee <andrew@andrewgee.org>
2015-12-21 16:24:20 +0000

    NISP-1419: Add nisp-frontend

--- jobs/nisp.groovy
+++ jobs/nisp.groovy
@@ -1,8 +1,12 @@
 import javaposse.jobdsl.dsl.DslFactory
 import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.BuildMonitorViewBuilder
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtMicroserviceJobBuilder
+import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtFrontendJobBuilder
 
 new SbtMicroserviceJobBuilder('nisp').
+                         build(this as DslFactory)
+                         
+new SbtFrontendJobBuilder('nisp-frontend').
                          build(this as DslFactory)
 
 new BuildMonitorViewBuilder('NISP-MONITOR')
