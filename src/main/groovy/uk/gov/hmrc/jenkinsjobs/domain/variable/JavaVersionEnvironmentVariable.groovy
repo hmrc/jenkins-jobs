6aa68ac7532847e30e8a95c73f7bee513a5501f7
Steve Smith <steve@alwaysagileconsulting.com>
2016-01-08 16:12:53 +0000

    PLATOPS-217 upgraded Java to 1.8.0_66

--- src/main/groovy/uk/gov/hmrc/jenkinsjobs/domain/variable/JavaVersionEnvironmentVariable.groovy
+++ src/main/groovy/uk/gov/hmrc/jenkinsjobs/domain/variable/JavaVersionEnvironmentVariable.groovy
@@ -5,7 +5,6 @@ import uk.gov.hmrc.jenkinsjobbuilders.domain.variable.EnvironmentVariable
 import static uk.gov.hmrc.jenkinsjobbuilders.domain.variable.StringEnvironmentVariable.stringEnvironmentVariable
 
 enum JavaVersionEnvironmentVariable implements EnvironmentVariable {
-    JDK8_40('1.8.0_40'),
     JDK8_66('1.8.0_66')
 
     private final EnvironmentVariable environmentVariable
