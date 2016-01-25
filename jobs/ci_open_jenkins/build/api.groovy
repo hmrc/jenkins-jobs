fc57757ee4e085c30109bfb68a66fbe2b55fe55c
Chinmay Naik <chinmay185@gmail.com>
2016-01-25 10:28:38 +0000

    add epaye-annual-summary job

--- jobs/ci_open_jenkins/build/api.groovy
+++ jobs/ci_open_jenkins/build/api.groovy
@@ -3,10 +3,15 @@ package ci_open_jenkins.build
 import javaposse.jobdsl.dsl.DslFactory
 import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.BuildMonitorViewBuilder
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtFrontendJobBuilder
+import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtMicroserviceJobBuilder
 
 new SbtFrontendJobBuilder('api-example-scala-client').
         build(this as DslFactory)
 
+new SbtMicroserviceJobBuilder('epaye-annual-summary').
+        withScalaStyle().
+        withSCoverage().
+        build(this as DslFactory)
 
 new BuildMonitorViewBuilder('API-MONITOR')
-        .withJobs('api-example-scala-client').build(this)
+        .withJobs('api-example-scala-client', 'epaye-annual-summary').build(this)
