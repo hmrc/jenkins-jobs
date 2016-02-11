74d27589d1fa460d54680b876351a4d36859c1ea
ashu.singh <asingh@equalexperts.com>
2016-02-11 11:31:34 +0000

    PLATOPS-358 [AS,CK] up the version of init-repository for github fix

--- jobs/ci_open_jenkins/build/platops.groovy
+++ jobs/ci_open_jenkins/build/platops.groovy
@@ -65,7 +65,7 @@ new SbtLibraryJobBuilder('sbt-distributables').
                          build(this)
 
 jobBuilder('create-a-repository').
-          withEnvironmentVariables(stringEnvironmentVariable('INIT_REPO_VERSION', '0.12.0')).
+          withEnvironmentVariables(stringEnvironmentVariable('INIT_REPO_VERSION', '0.13.0')).
           withParameters(stringParameter('REPOSITORY_NAME','','The repository name e.g. foo-frontend')).
           withParameters(stringParameter('TEAM_NAME','','The exact name of the github team to which the repository will be added')).          
           withParameters(choiceParameter('REPOSITORY_TYPE',['Sbt','SbtPlugin'],'The repository type e.g. SBT')).
