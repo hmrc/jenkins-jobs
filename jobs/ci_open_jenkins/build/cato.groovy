fb4000591be203f9e255713a802e243044307037
Allen Fensome <allen.fensome@gmail.com>
2016-03-09 15:19:12 +0000

    Add cato-runtime-utils

--- jobs/ci_open_jenkins/build/cato.groovy
+++ jobs/ci_open_jenkins/build/cato.groovy
@@ -13,8 +13,12 @@ new SbtLibraryJobBuilder('attachments-client').
 new SbtLibraryJobBuilder('ct-calculations').
         build(this as DslFactory)
 
+new SbtLibraryJobBuilder('cato-runtime-utils').
+        build(this as DslFactory)
+
 new BuildMonitorViewBuilder('CATO-MONITOR')
 .withJobs('ct-calculations',
           'play-events',
-          'attachments-client'
+          'attachments-client',
+          'cato-runtime-utils'
         ).build(this)
