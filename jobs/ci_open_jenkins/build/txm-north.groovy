e07cd75c2b2eeda7d6a4d853e24a3c22d6ea17e5
Rick <rickb777@github.com>
2016-03-17 16:08:18 +0000

    Added address-reputation-store and ingester

--- jobs/ci_open_jenkins/build/txm-north.groovy
+++ jobs/ci_open_jenkins/build/txm-north.groovy
@@ -2,19 +2,21 @@ package ci_open_jenkins.build
 
 import javaposse.jobdsl.dsl.DslFactory
 import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.BuildMonitorViewBuilder
+import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtLibraryJobBuilder
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtMicroserviceJobBuilder
-import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtFrontendJobBuilder
 
-new SbtMicroserviceJobBuilder('address-lookup-ingestor').
+new SbtLibraryJobBuilder('address-reputation-store').build(this as DslFactory)
+
+new SbtMicroserviceJobBuilder('address-reputation-ingester').
         withScalaStyle().
         withSCoverage().
         build(this as DslFactory)
-
+                         
 new SbtMicroserviceJobBuilder('address-lookup-demo').
         withScalaStyle().
         withSCoverage().
         build(this as DslFactory)
                          
 new BuildMonitorViewBuilder('TXM-NORTH-MONITOR')
-        .withJobs('address-lookup-ingestor', 'address-lookup-demo').build(this)
+        .withJobs('address-reputation-store', 'address-reputation-ingester', 'address-lookup-demo').build(this)
 
