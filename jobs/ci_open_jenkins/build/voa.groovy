ed768737a4e73dffb80a99d489f9c5e9076c0db2
James Shade <james.shade@digital.voa.gov.uk>
2016-03-22 14:38:33 +0000

    Added cca-challenge project.

--- jobs/ci_open_jenkins/build/voa.groovy
+++ jobs/ci_open_jenkins/build/voa.groovy
@@ -3,9 +3,15 @@ package ci_open_jenkins.build
 import javaposse.jobdsl.dsl.DslFactory
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtLibraryJobBuilder
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtFrontendJobBuilder
+import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtMicroserviceJobBuilder
 
 new SbtLibraryJobBuilder('play-conditional-form-mapping').
         build(this as DslFactory)
 
 new SbtFrontendJobBuilder('for-frontend')
         .build(this as DslFactory)
+
+new SbtMicroserviceJobBuilder('cca-challenge').withTests("test")
+       .build(this as DslFactory)
+
+
