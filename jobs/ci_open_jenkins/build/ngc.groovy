f501d07d12aa20e844665da35bbc58c57f682dc6
Duncan Crawford <duncan@noughtsandones.co.uk>
2016-04-04 22:33:08 +0100

    addition of ngc jobs

---
+++ jobs/ci_open_jenkins/build/ngc.groovy
@@ -0,0 +1,17 @@
+package ci_open_jenkins.build
+
+import javaposse.jobdsl.dsl.DslFactory
+import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.BuildMonitorViewBuilder
+import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtFrontendJobBuilder
+import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtLibraryJobBuilder
+import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtMicroserviceJobBuilder
+
+new SbtMicroserviceJobBuilder('next-generation-consumer').build(this as DslFactory)
+
+new SbtMicroserviceJobBuilder('customer-profile').build(this as DslFactory)
+
+new SbtFrontendJobBuilder('taint').build(this as DslFactory)
+
+new BuildMonitorViewBuilder('NGC-MONITOR')
+        .withJobs('next-generation-consumer','taint').build(this)
+
