9f1b49a83e8bb3ebc9f87cae416ebe266ab61210
Thomas VANDEVELDE <thomas.vandevelde@digital.hmrc.gov.uk>
2016-04-06 11:55:48 +0100

    API-1213 - Add api-revocation-frontend

--- jobs/ci_open_jenkins/build/api.groovy
+++ jobs/ci_open_jenkins/build/api.groovy
@@ -1,14 +1,18 @@
 package ci_open_jenkins.build
-
 import javaposse.jobdsl.dsl.DslFactory
 import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.BuildMonitorViewBuilder
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtFrontendJobBuilder
-import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtMicroserviceJobBuilder
 
 new SbtFrontendJobBuilder('api-example-scala-client').
         build(this as DslFactory)
 
 new SbtFrontendJobBuilder('api-gatekeeper-frontend').
+        withTests("test acceptance:test").
+        withHtmlReports('target/acceptance-test-reports/': 'Acceptance tests HTML Report').
+        withXvfb().
+        build(this as DslFactory)
+
+new SbtFrontendJobBuilder('api-revocation-frontend').
         withTests("test acceptance:test").
         withHtmlReports('target/acceptance-test-reports/': 'Acceptance tests HTML Report').
         withXvfb().
