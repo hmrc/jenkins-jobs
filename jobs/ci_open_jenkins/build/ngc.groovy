ad4a58eaba9467207ee19b336d19e41e2dae8a2f
Duncan Crawford <duncan@noughtsandones.co.uk>
2016-04-07 14:01:47 +0100

    update ngc jobs with mobile-messages
    remove next-generation-consumer as it never got created

--- jobs/ci_open_jenkins/build/ngc.groovy
+++ jobs/ci_open_jenkins/build/ngc.groovy
@@ -6,12 +6,14 @@ import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtFrontendJobBuilder
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtLibraryJobBuilder
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtMicroserviceJobBuilder
 
-new SbtMicroserviceJobBuilder('next-generation-consumer').build(this as DslFactory)
-
 new SbtMicroserviceJobBuilder('customer-profile').build(this as DslFactory)
 
 new SbtFrontendJobBuilder('personal-income').build(this as DslFactory)
 
+new SbtFrontendJobBuilder('mobile-messages').build(this as DslFactory)
+
 new BuildMonitorViewBuilder('NGC-MONITOR')
-        .withJobs('next-generation-consumer','personal-income').build(this)
+        .withJobs('personal-income', 
+        	'customer-profile',
+        	'mobile-messages').build(this)
 
