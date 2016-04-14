5fa8eb945bb7cc0ce80b37513b9f3bcfe91a4f10
alex.nakos <alex.nakos@digital.hmrc.gov.uk>
2016-04-14 14:51:06 +0100

    DC-348: adding updated-print-suppressions microservice job

--- jobs/ci_open_jenkins/build/digital-contact.groovy
+++ jobs/ci_open_jenkins/build/digital-contact.groovy
@@ -3,6 +3,7 @@ package ci_open_jenkins.build
 import javaposse.jobdsl.dsl.DslFactory
 import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.BuildMonitorViewBuilder
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtLibraryJobBuilder
+import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtMicroserviceJobBuilder
 
 new SbtLibraryJobBuilder('a-b-test').
         build(this as DslFactory)
@@ -22,11 +23,15 @@ new SbtLibraryJobBuilder('batch-updater').
 new SbtLibraryJobBuilder('work-item-repo').
         build(this)
 
+new SbtMicroserviceJobBuilder('updated-print-suppressions').
+        build(this as DslFactory)
+
 new BuildMonitorViewBuilder('DC-MONITOR')
         .withJobs('a-b-test',
                   'url-builder',
                   'emailaddress',
                   'play-scheduling',
                   'batch-updater',
-                  'work-item-repo'
+                  'work-item-repo',
+                  'updated-print-suppressions'
                   ).build(this)
