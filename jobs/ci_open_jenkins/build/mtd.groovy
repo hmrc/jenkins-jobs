f586ff8bc3498f96e96bfebfca0c6d97673e157e
Chandrashekhar Korivi <Chandrashekhar.Korivi@digital.hmrc.gov.uk>
2016-04-18 12:40:02 +0100

    [MTDSA-187] [CK,DA,SA] Create the jenkins job for self-assessment-api micro service

---
+++ jobs/ci_open_jenkins/build/mtd.groovy
@@ -0,0 +1,12 @@
+package ci_open_jenkins.build
+
+import javaposse.jobdsl.dsl.DslFactory
+import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.BuildMonitorViewBuilder
+import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtLibraryJobBuilder
+
+new SbtLibraryJobBuilder('self-assessment-api').
+        build(this as DslFactory)
+
+new BuildMonitorViewBuilder('MTD-MONITOR')
+        .withJobs('self-assessment-api').build(this)
+
