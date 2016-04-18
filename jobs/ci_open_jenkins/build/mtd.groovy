c82c73e133d45996ef94973ac29bb28fdee9bd06
Chandrashekhar Korivi <Chandrashekhar.Korivi@digital.hmrc.gov.uk>
2016-04-18 13:38:10 +0100

    [MTDSA-187] [CK,DA,SA] Update the job to use micro service builder instead of library builder

--- jobs/ci_open_jenkins/build/mtd.groovy
+++ jobs/ci_open_jenkins/build/mtd.groovy
@@ -2,9 +2,9 @@ package ci_open_jenkins.build
 
 import javaposse.jobdsl.dsl.DslFactory
 import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.BuildMonitorViewBuilder
-import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtLibraryJobBuilder
+import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtMicroserviceJobBuilder
 
-new SbtLibraryJobBuilder('self-assessment-api').
+new SbtMicroserviceJobBuilder('self-assessment-api').
         build(this as DslFactory)
 
 new BuildMonitorViewBuilder('MTD-MONITOR')
