96694139584968be46baff8d275a60d5acc21319
Adam Conder <adamdconder@gmail.com>
2016-04-20 14:53:42 +0100

    adding in cb-submission microservice

--- jobs/ci_open_jenkins/build/cb.groovy
+++ jobs/ci_open_jenkins/build/cb.groovy
@@ -5,10 +5,15 @@ import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.BuildMonitorViewBuilder
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtMicroserviceJobBuilder
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtFrontendJobBuilder
 
+new SbtMicroserviceJobBuilder('cb-submission').
+        withScalaStyle().
+        withSCoverage().
+        build(this as DslFactory)
+
 new SbtFrontendJobBuilder('cb-frontend').
         withScalaStyle().
         withSCoverage().
         build(this as DslFactory)
 
 new BuildMonitorViewBuilder('CB-MONITOR')
-        .withJobs('cb-frontend').build(this)
+        .withJobs('cb-submission', 'cb-frontend').build(this)
