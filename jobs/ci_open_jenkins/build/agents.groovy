9c671b780708ef98ca90dd748d09ca93b5686d87
Darren Morris <dmorris@equalexperts.com>
2016-04-20 15:47:18 +0100

    renamed to follow naming conventions

--- jobs/ci_open_jenkins/build/agents.groovy
+++ jobs/ci_open_jenkins/build/agents.groovy
@@ -4,5 +4,5 @@ import javaposse.jobdsl.dsl.DslFactory
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtLibraryJobBuilder
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtMicroserviceJobBuilder
 
-new SbtMicroserviceJobBuilder('agent-sa').build(this as DslFactory)
+new SbtMicroserviceJobBuilder('agent-self-assessment').build(this as DslFactory)
 
