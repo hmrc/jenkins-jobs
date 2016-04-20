453a993057e0858ee373642dad17a85d4a2e446b
Darren Morris <dmorris@equalexperts.com>
2016-04-20 15:35:42 +0100

    Added agent-sa build

---
+++ jobs/ci_open_jenkins/build/agents.groovy
@@ -0,0 +1,8 @@
+package ci_open_jenkins.build
+
+import javaposse.jobdsl.dsl.DslFactory
+import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtLibraryJobBuilder
+import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtMicroserviceJobBuilder
+
+new SbtMicroserviceJobBuilder('agent-sa').build(this as DslFactory)
+
