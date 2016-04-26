ebbf61d155a061d0b1eaf9f16a17c90edf83b074
thomas.stacey <thomas.stacey@digital.hmrc.gov.uk>
2016-04-26 10:33:06 +0100

    PLA-165 Add pla job builder

---
+++ jobs/ci_open_jenkins/build/pla.groovy
@@ -0,0 +1,9 @@
+package ci_open_jenkins.build
+
+import javaposse.jobdsl.dsl.DslFactory
+import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtFrontendJobBuilder
+
+new SbtFrontendJobBuilder('pensions-lifetime-allowance-frontend').
+        withScalaStyle().
+        withSCoverage().
+        build(this as DslFactory)
