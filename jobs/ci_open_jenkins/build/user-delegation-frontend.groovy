76efbc881700f4c9998d1420762f5d2440e5f5a4
Paolo Boni <paolo.boni@digital.hmrc.gov.uk>
2016-05-03 15:34:36 +0100

    TSV-58 TSV-59: Moved user-delegation and user-delegation-frontend to ci-dev.

--- jobs/ci_open_jenkins/build/user-delegation-frontend.groovy
+++
@@ -1,7 +0,0 @@
-package ci_open_jenkins.build
-
-import javaposse.jobdsl.dsl.DslFactory
-import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtFrontendJobBuilder
-
-new SbtFrontendJobBuilder('user-delegation-frontend').
-                         build(this as DslFactory)
--- jobs/ci_open_jenkins/build/user-delegation.groovy
+++
@@ -1,7 +0,0 @@
-package ci_open_jenkins.build
-
-import javaposse.jobdsl.dsl.DslFactory
-import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtMicroserviceJobBuilder
-
-new SbtMicroserviceJobBuilder('user-delegation').
-                         build(this as DslFactory)
