931e396436c9ea1556b05d287c85ec46e9f39a88
Chandrashekhar Korivi <Chandrashekhar.Korivi@digital.hmrc.gov.uk>
2016-05-05 10:56:02 +0100

    [MTDSA-191] [SA|CK] Add an environment property to identify whether to use disk mongo or embedded mongo

--- jobs/ci_open_jenkins/build/mtd.groovy
+++ jobs/ci_open_jenkins/build/mtd.groovy
@@ -1,11 +1,28 @@
 package ci_open_jenkins.build
 
 import javaposse.jobdsl.dsl.DslFactory
+import javaposse.jobdsl.dsl.Job
 import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.BuildMonitorViewBuilder
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtMicroserviceJobBuilder
 
-new SbtMicroserviceJobBuilder('self-assessment-api').
-        build(this as DslFactory)
+import static uk.gov.hmrc.jenkinsjobbuilders.domain.variable.StringEnvironmentVariable.stringEnvironmentVariable
+
+class MtdSbtMicroserviceJobBuilder {
+    private final SbtMicroserviceJobBuilder delegateJob
+
+    public MtdSbtMicroserviceJobBuilder(SbtMicroserviceJobBuilder jobBuilder) {
+        this.delegateJob = jobBuilder
+    }
+
+    Job build(DslFactory dslFactory) {
+        delegateJob.jobBuilder.withEnvironmentVariables(stringEnvironmentVariable("USE_DISK_MONGO", "TRUE"))
+        delegateJob.build(dslFactory)
+    }
+}
+
+new MtdSbtMicroserviceJobBuilder(new SbtMicroserviceJobBuilder('self-assessment-api'))
+        .build(this as DslFactory)
+
 
 new BuildMonitorViewBuilder('MTD-MONITOR')
         .withJobs('self-assessment-api').build(this)
