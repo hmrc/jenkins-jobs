fccbb21f1df3a81f41ff1df348b5c35f278bc271
Pete Smith <pete.smith@digital.hmrc.gov.uk>
2016-05-09 15:37:33 +0100

    [PS] Renamed catalogue (api) to teams-and-services

--- jobs/ci_open_jenkins/build/platops.groovy
+++ jobs/ci_open_jenkins/build/platops.groovy
@@ -125,7 +125,7 @@ jobBuilder('clean-slaves').
            build(this)
 
 
-new SbtMicroserviceJobBuilder('catalogue').withTests("test")
+new SbtMicroserviceJobBuilder('teams-and-services').withTests("test")
        .build(this as DslFactory)
 
 new SbtMicroserviceJobBuilder('catalogue-frontend').withTests("test")
@@ -133,11 +133,8 @@ new SbtMicroserviceJobBuilder('catalogue-frontend').withTests("test")
 
 new SbtLibraryJobBuilder('alert-config-builder').build(this as DslFactory)
 
-new SbtLibraryJobBuilder('git-client').build(this as DslFactory)
-new SbtLibraryJobBuilder('github-client').build(this as DslFactory)
-
 new SbtMicroserviceJobBuilder('indicators').withTests("test")
         .build(this as DslFactory)
 
 new BuildMonitorViewBuilder('PLATOPS-MONITOR')
-        .withJobs('sbt-git-versioning', 'time', 'sbt-bobby', 'jenkins-job-builders', 'git-stamp', 'init-repository', 'releaser', 'govuk-template', 'sbt-bintray-publish', 'sbt-auto-build', 'sbt-git-stamp', 'sbt-settings', 'sbt-distributables', 'catalogue', 'catalogue-frontend', 'alert-config-builder', 'init-service', 'indicators', 'create-a-release', 'create-a-repository', 'create-a-webhook').build(this)
+        .withJobs('sbt-git-versioning', 'time', 'sbt-bobby', 'jenkins-job-builders', 'git-stamp', 'init-repository', 'releaser', 'govuk-template', 'sbt-bintray-publish', 'sbt-auto-build', 'sbt-git-stamp', 'sbt-settings', 'sbt-distributables', 'teams-and-services', 'catalogue-frontend', 'alert-config-builder', 'init-service', 'indicators', 'create-a-release', 'create-a-repository', 'create-a-webhook').build(this)
