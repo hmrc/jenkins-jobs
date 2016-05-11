3c31a26df7d4892c5125b92eedb87115fd32162c
Pete Smith <pete.smith@digital.hmrc.gov.uk>
2016-05-11 10:43:27 +0100

    [PS] Reinstated missing github-client job

--- jobs/ci_open_jenkins/build/platops.groovy
+++ jobs/ci_open_jenkins/build/platops.groovy
@@ -136,5 +136,7 @@ new SbtLibraryJobBuilder('alert-config-builder').build(this as DslFactory)
 new SbtMicroserviceJobBuilder('indicators').withTests("test")
         .build(this as DslFactory)
 
+new SbtLibraryJobBuilder('github-client').build(this as DslFactory)
+
 new BuildMonitorViewBuilder('PLATOPS-MONITOR')
-        .withJobs('sbt-git-versioning', 'time', 'sbt-bobby', 'jenkins-job-builders', 'git-stamp', 'init-repository', 'releaser', 'govuk-template', 'sbt-bintray-publish', 'sbt-auto-build', 'sbt-git-stamp', 'sbt-settings', 'sbt-distributables', 'teams-and-services', 'catalogue-frontend', 'alert-config-builder', 'init-service', 'indicators', 'create-a-release', 'create-a-repository', 'create-a-webhook').build(this)
+        .withJobs('sbt-git-versioning', 'time', 'sbt-bobby', 'jenkins-job-builders', 'git-stamp', 'init-repository', 'releaser', 'govuk-template', 'sbt-bintray-publish', 'sbt-auto-build', 'sbt-git-stamp', 'sbt-settings', 'sbt-distributables', 'teams-and-services', 'catalogue-frontend', 'alert-config-builder', 'init-service', 'indicators', 'create-a-release', 'create-a-repository', 'create-a-webhook', 'github-client').build(this)
