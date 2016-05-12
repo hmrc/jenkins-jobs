02e23fd0b919e6b055aecdf979037106e26cc7c3
christopher.walker <christopher.walker@digital.hmrc.gov.uk>
2016-05-12 16:31:51 +0100

    Added business-registration-frontend to brf.groovy

---
+++ jobs/ci_open_jenkins/build/brf.groovy
@@ -0,0 +1,9 @@
+package ci_open_jenkins.build
+
+import javaposse.jobdsl.dsl.DslFactory
+import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.BuildMonitorViewBuilder
+import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtFrontendJobBuilder
+import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtLibraryJobBuilder
+import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtMicroserviceJobBuilder
+
+new SbtFrontendJobBuilder('business-registration-frontend').build(this as DslFactory)
