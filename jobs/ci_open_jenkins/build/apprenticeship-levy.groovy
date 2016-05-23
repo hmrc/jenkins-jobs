7ddecae8c2c52ae4394d8b5c2c3b96da56091b2d
Doug Clinton <doug@wiredthing.com>
2016-05-23 11:06:26 +0100

    Changed apprenticeship-levy to be a Microservice rather than a Frontend and added the stub builder.

--- jobs/ci_open_jenkins/build/apprenticeship-levy.groovy
+++ jobs/ci_open_jenkins/build/apprenticeship-levy.groovy
@@ -1,8 +1,15 @@
 package ci_open_jenkins.build
 import javaposse.jobdsl.dsl.DslFactory
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtFrontendJobBuilder
+import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtMicroserviceJobBuilder
 
 
-
-new SbtFrontendJobBuilder('apprenticeship-levy').
+new SbtMicroserviceJobBuilder('apprenticeship-levy').
+	withScalaStyle().
+	withSCoverage().
         build(this as DslFactory)
+	
+new SbtMicroserviceJobBuilder('apprenticeship-levy-stub').
+        withScalaStyle().
+        withSCoverage().
+        build(this as DslFactory)	
