d8e11480b526d2344b15cc1afca8e39df0f24320
Francis Devereux <francis.devereux@digital.hmrc.gov.uk>
2016-05-23 17:09:10 +0100

    [FD] AOSS-1293 Enable scoverage and scalastyle for agent-access-control

--- jobs/ci_open_jenkins/build/agents.groovy
+++ jobs/ci_open_jenkins/build/agents.groovy
@@ -3,4 +3,7 @@ package ci_open_jenkins.build
 import javaposse.jobdsl.dsl.DslFactory
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtMicroserviceJobBuilder
 
-new SbtMicroserviceJobBuilder('agent-access-control').build(this as DslFactory)
+new SbtMicroserviceJobBuilder('agent-access-control').
+        withSCoverage().
+        withScalaStyle().
+        build(this as DslFactory)
