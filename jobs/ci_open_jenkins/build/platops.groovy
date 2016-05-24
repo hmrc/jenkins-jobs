1a3ffa96c82a267226aebd1c81a60f8d7acc0c7f
Steve Smith <steve@alwaysagileconsulting.com>
2016-05-24 10:57:10 +0100

    SS/AS Add missing git-client job

--- jobs/ci_open_jenkins/build/platops.groovy
+++ jobs/ci_open_jenkins/build/platops.groovy
@@ -138,5 +138,7 @@ new SbtMicroserviceJobBuilder('indicators').withTests("test")
 
 new SbtLibraryJobBuilder('github-client').build(this as DslFactory)
 
+new SbtLibraryJobBuilder('git-client').build(this as DslFactory)
+
 new BuildMonitorViewBuilder('PLATOPS-MONITOR')
         .withJobs('sbt-git-versioning', 'time', 'sbt-bobby', 'jenkins-job-builders', 'git-stamp', 'init-repository', 'releaser', 'govuk-template', 'sbt-bintray-publish', 'sbt-auto-build', 'sbt-git-stamp', 'sbt-settings', 'sbt-distributables', 'teams-and-services', 'catalogue-frontend', 'alert-config-builder', 'init-service', 'indicators', 'create-a-release', 'create-a-repository', 'create-a-webhook', 'github-client').build(this)
