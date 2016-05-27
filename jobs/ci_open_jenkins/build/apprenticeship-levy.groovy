4b3c9147b013b1771c27de0d4f12699842830207
Baber Khalil <Baber.s.Khalil@googlemail.com>
2016-05-27 11:51:02 +0100

    - added build monitor for apprenticeship levy.

--- jobs/ci_open_jenkins/build/apprenticeship-levy.groovy
+++ jobs/ci_open_jenkins/build/apprenticeship-levy.groovy
@@ -2,10 +2,15 @@ package ci_open_jenkins.build
 import javaposse.jobdsl.dsl.DslFactory
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtFrontendJobBuilder
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtMicroserviceJobBuilder
+import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.BuildMonitorViewBuilder
 
 
 new SbtMicroserviceJobBuilder('apprenticeship-levy').
         build(this as DslFactory)
-	
+
 new SbtMicroserviceJobBuilder('apprenticeship-levy-stub').
-        build(this as DslFactory)	
+        build(this as DslFactory)
+
+new BuildMonitorViewBuilder('APPRENTICESHIP-LEVY-MONITOR')
+        .withJobs('apprenticeship-levy',
+        'apprenticeship-levy-stub').build(this)
