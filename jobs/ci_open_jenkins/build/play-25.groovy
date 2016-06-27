a4024d48c5df51fa837abfe6b4464ad597689660
franek <franek.richardson@digital.hmrc.gov.uk>
2016-06-27 16:23:56 +0100

    Add job definitions for play 2.5 spike libraries

--- jobs/ci_open_jenkins/build/play-25.groovy
+++ jobs/ci_open_jenkins/build/play-25.groovy
@@ -4,12 +4,6 @@ import javaposse.jobdsl.dsl.DslFactory
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtLibraryJobBuilder
 
 //play 2.5 jobs
-new SbtLibraryJobBuilder('http-verbs-25').
-        build(this as DslFactory)
-
-new SbtLibraryJobBuilder('play-auditing-25').
-        build(this as DslFactory)
-
 new SbtLibraryJobBuilder('sbt-distributables-25').
                          withoutJUnitReports().
                          build(this)
@@ -17,6 +11,20 @@ new SbtLibraryJobBuilder('sbt-distributables-25').
 new SbtLibraryJobBuilder('sbt-distributables-plugin-25').
                          withoutJUnitReports().
                          build(this)
+
+new SbtLibraryJobBuilder('sbt-auto-build-25').
+                         withoutJUnitReports().
+                        build(this as DslFactory)
+
+new SbtLibraryJobBuilder('sbt-git-versioning-25').
+                        withoutJUnitReports().
+                        build(this as DslFactory)
+
+new SbtLibraryJobBuilder('http-verbs-25').
+        build(this as DslFactory)
+
+new SbtLibraryJobBuilder('play-auditing-25').
+        build(this as DslFactory)
 
 new SbtLibraryJobBuilder('play-health-25').
         build(this as DslFactory)
@@ -33,5 +41,17 @@ new SbtLibraryJobBuilder('play-graphite-25').
         build(this as DslFactory)
 
 new SbtLibraryJobBuilder('play-url-binders-25').
+        build(this as DslFactory)
+
+new SbtLibraryJobBuilder('frontend-bootstrap-25').
+        build(this as DslFactory)
+
+new SbtLibraryJobBuilder('hmrctest-25').
+        build(this as DslFactory)
+
+new SbtLibraryJobBuilder('microservice-bootstrap-25').
+        build(this as DslFactory)
+
+new SbtLibraryJobBuilder('play-filters-25').
         build(this as DslFactory)
 
