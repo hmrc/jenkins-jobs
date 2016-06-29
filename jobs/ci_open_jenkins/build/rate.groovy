7c60762507dc99bafa511e4b0e65de5d00059236
Dave Joss <dave.joss@digital.hmrc.gov.uk>
2016-06-29 10:17:52 +0100

    RATE-281 added RATE groovy file

---
+++ jobs/ci_open_jenkins/build/rate.groovy
@@ -0,0 +1,14 @@
+package ci_open_jenkins.build
+
+import javaposse.jobdsl.dsl.DslFactory
+import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtFrontendJobBuilder
+import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.BuildMonitorViewBuilder
+import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtMicroserviceJobBuilder
+
+new SbtMicroserviceJobBuilder('back-office-adapter').
+                 withScalaStyle().
+                 withSCoverage().
+        build(this as DslFactory)
+
+new BuildMonitorViewBuilder('RATE-MONITOR')
+        .withJobs('back-office-adapter').build(this)
