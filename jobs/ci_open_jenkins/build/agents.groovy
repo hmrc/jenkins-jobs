e6ce2b36ef6a0aa28721481de7ceef0dc0c07104
Ben Letton <ben.letton@digital.hmrc.gov.uk>
2016-07-11 13:36:55 +0100

    AOSS-1435 BL switched the agent-client-authorisation-frontend build to use the SbtFrontendBuilder

--- jobs/ci_open_jenkins/build/agents.groovy
+++ jobs/ci_open_jenkins/build/agents.groovy
@@ -2,6 +2,7 @@ package ci_open_jenkins.build
 
 import javaposse.jobdsl.dsl.DslFactory
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtMicroserviceJobBuilder
+import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtFrontendJobBuilder
 
 new SbtMicroserviceJobBuilder('agent-access-control').
         withSCoverage().
@@ -12,4 +13,7 @@ new SbtMicroserviceJobBuilder('agent-client-authorisation').
         build(this as DslFactory)
 
 new SbtMicroserviceJobBuilder('agent-client-authorisation-frontend').
+        build(this as DslFactory)
+
+new SbtFrontendJobBuilder('agent-client-authorisation-frontend'). 
         build(this as DslFactory)
