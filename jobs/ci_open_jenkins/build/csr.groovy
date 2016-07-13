b98ff2e21357db7113d9a26de7b2ab498601cda4
Oliver Wickham <mr.o.wickham@gmail.com>
2016-07-13 15:39:47 +0100

    Add csr-faststream jobs

--- jobs/ci_open_jenkins/build/csr.groovy
+++ jobs/ci_open_jenkins/build/csr.groovy
@@ -5,12 +5,21 @@ import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.BuildMonitorViewBuilder
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtFrontendJobBuilder
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtMicroserviceJobBuilder
 
+
+new SbtMicroserviceJobBuilder('csr-faststream').
+    build(this as DslFactory)
+    
+new SbtFrontendJobBuilder('csr-faststream-frontend').
+    build(this as DslFactory)
+
+
 new SbtMicroserviceJobBuilder('csr-fasttrack').
     build(this as DslFactory)
 
 new SbtFrontendJobBuilder('csr-fasttrack-frontend').
     build(this as DslFactory)
 
+
 new BuildMonitorViewBuilder('CSR-MONITOR').
-    withJobs('csr-fasttrack', 'csr-fasttrack-frontend').
+    withJobs('csr-faststream', 'csr-faststream-frontend', 'csr-fasttrack', 'csr-fasttrack-frontend').
     build(this)
