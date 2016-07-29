40e9171faa2416ff87d85576d983581ea19d13e1
Jakub Chrobasik <jakub.chrobasik@digital.voa.gov.uk>
2016-07-29 17:02:36 +0200

    Fixed name of one of the business-rates' jobs

--- jobs/ci_open_jenkins/build/business-rates.groovy
+++ jobs/ci_open_jenkins/build/business-rates.groovy
@@ -8,6 +8,6 @@ new SbtLibraryJobBuilder('valuetype')
         .withSCoverage()
         .build(this as DslFactory)
 
-new BuildMonitorViewBuilder('Business-Rates-MONITOR')
+new BuildMonitorViewBuilder('Business-Rates-Monitor')
         .withJobs('valuetype')
         .build(this)
