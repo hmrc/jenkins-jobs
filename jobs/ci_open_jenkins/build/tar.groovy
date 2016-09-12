da8a298567ec2eb10bc8ebc15a3e9106cad8e5de
Anshul Bajpai <anshul.bajpai@digital.hmrc.gov.uk>
2016-09-12 12:23:04 +0100

    Using installed chromedriver for TAR build

--- jobs/ci_open_jenkins/build/tar.groovy
+++ jobs/ci_open_jenkins/build/tar.groovy
@@ -4,12 +4,25 @@ import javaposse.jobdsl.dsl.DslFactory
 import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.BuildMonitorViewBuilder
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtFrontendJobBuilder
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtLibraryJobBuilder
+import static uk.gov.hmrc.jenkinsjobs.domain.publisher.Publishers.*
+import static uk.gov.hmrc.jenkinsjobs.domain.step.Steps.sbtCleanDistTgzPublish
+import static uk.gov.hmrc.jenkinsjobbuilders.domain.wrapper.AbsoluteTimeoutWrapper.timeoutWrapper
+import static uk.gov.hmrc.jenkinsjobs.domain.builder.JobBuilders.jobBuilder
+import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.JobBuilder
 
-new SbtFrontendJobBuilder('tax-account-router-frontend').
-                         build(this as DslFactory)
+def microserviceName = "tax-account-router-frontend"
+
+jobBuilder = jobBuilder(microserviceName, microserviceName, 'master').
+        withPublishers(defaultHtmlReportsPublisher(),
+                defaultBuildDescriptionPublisher(),
+                defaultJUnitReportsPublisher(),
+                bobbyArtifactsPublisher())
+        .withSteps(sbtCleanDistTgzPublish("-Dwebdriver.chrome.driver=/usr/local/bin/chromedriver","test it:test", "")).
+        withWrappers(timeoutWrapper(15)).
+        build(this)
 
 new BuildMonitorViewBuilder('TAR-MONITOR')
-        .withJobs('tax-account-router-frontend').build(this)
+        .withJobs(microserviceName).build(this)
 
 new SbtLibraryJobBuilder('performance-test-runner')
         .build(this as DslFactory)
