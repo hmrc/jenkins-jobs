69e844cdb7f32483312ac59ece2c4aa863f20568
ankit.singhal <ankit.singhal@digital.hmrc.gov.uk>
2016-09-28 14:50:14 +0100

    Create Trust Registration Job in open

--- jobs/ci_open_jenkins/build/trust-registration.groovy
+++ jobs/ci_open_jenkins/build/trust-registration.groovy
@@ -4,10 +4,10 @@ import javaposse.jobdsl.dsl.DslFactory
 import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.BuildMonitorViewBuilder
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtFrontendJobBuilder
 
-new SbtFrontendJobBuilder('trust-registration-frontend').
+new SbtFrontendJobBuilder('trust-registration-api').
         withScalaStyle().
         withSCoverage().
         build(this as DslFactory)
 
 new BuildMonitorViewBuilder('TRUST-MONITOR')
-        .withJobs('trust-registration-frontend').build(this)
+        .withJobs('trust-registration-api').build(this)
