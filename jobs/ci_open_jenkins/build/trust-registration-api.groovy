89c0372380efa546ea56e57531736a8b43b89f4d
ankit.singhal <ankit.singhal@digital.hmrc.gov.uk>
2016-09-30 14:26:04 +0100

    change the job type in trust registration api

--- jobs/ci_open_jenkins/build/trust-registration-api.groovy
+++ jobs/ci_open_jenkins/build/trust-registration-api.groovy
@@ -2,9 +2,9 @@ package ci_open_jenkins.build
 
 import javaposse.jobdsl.dsl.DslFactory
 import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.BuildMonitorViewBuilder
-import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtFrontendJobBuilder
+import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtMicroserviceJobBuilder
 
-new SbtFrontendJobBuilder('trust-registration-api').
+new SbtMicroserviceJobBuilder('trust-registration-api').
         withScalaStyle().
         withSCoverage().
         build(this as DslFactory)
