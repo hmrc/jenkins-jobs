601dbf5821077f9728d54f0b86276813a8e82349
shridhaarchippaa <shridhaar.chippaa@accenture.com>
2016-10-03 11:23:05 +0100

    Added only with tests for new childcare schemes frontend

--- jobs/ci_open_jenkins/build/new-childcare-schemes.groovy
+++ jobs/ci_open_jenkins/build/new-childcare-schemes.groovy
@@ -1,14 +1,12 @@
 package ci_open_jenkins.build
-
 import javaposse.jobdsl.dsl.DslFactory
 import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.BuildMonitorViewBuilder
-import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtMicroserviceJobBuilder
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtFrontendJobBuilder
 
-                         
 new SbtFrontendJobBuilder('new-childcare-schemes-frontend').
         withScalaStyle().
         withSCoverage().
+        withTests("test").
         build(this as DslFactory)
 
 
