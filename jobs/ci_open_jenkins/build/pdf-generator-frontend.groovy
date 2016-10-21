3beca79fcf2b6b775a86721c30bea81bd94360a4
habeeb <habeeb.olayemi@digital.hmrc.gov.uk>
2016-10-21 15:24:45 +0100

    OFFPAY-95: corrected entry for pdf-generator-frontend

--- jobs/ci_open_jenkins/build/pdf-generator-frontend.groovy
+++ jobs/ci_open_jenkins/build/pdf-generator-frontend.groovy
@@ -2,7 +2,7 @@ package ci_open_jenkins.build
 
 import javaposse.jobdsl.dsl.DslFactory
 import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.BuildMonitorViewBuilder
-import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtMicroserviceJobBuilder
+import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtFrontendJobBuilder
 
 new SbtFrontendJobBuilder('pdf-generator-frontend').
         withScalaStyle().
