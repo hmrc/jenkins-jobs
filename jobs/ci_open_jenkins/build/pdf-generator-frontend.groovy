c7c3a72b7bb62ce9c930f16fed0f51ddfc881ba5
habeeb <habeeb.olayemi@digital.hmrc.gov.uk>
2016-10-21 15:17:04 +0100

    corrected entry for pdf-generator-frontend

--- jobs/ci_open_jenkins/build/pdf-generator-frontend.groovy
+++ jobs/ci_open_jenkins/build/pdf-generator-frontend.groovy
@@ -3,9 +3,8 @@ package ci_open_jenkins.build
 import javaposse.jobdsl.dsl.DslFactory
 import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.BuildMonitorViewBuilder
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtMicroserviceJobBuilder
-import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtFrontendJobBuilder
 
-new SbtMicroserviceJobBuilder('pdf-generator-frontend').
+new SbtFrontendJobBuilder('pdf-generator-frontend').
         withScalaStyle().
         withSCoverage().
         build(this as DslFactory)
