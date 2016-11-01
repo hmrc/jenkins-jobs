1ec28a842c0c6583d57012af383a41fb7b030575
ashu.singh <asingh@equalexperts.com>
2016-11-01 16:14:01 +0000

    SUP-6324 changed back the java version

--- src/main/groovy/uk/gov/hmrc/jenkinsjobs/domain/variable/JavaVersionEnvironmentVariable.groovy
+++ src/main/groovy/uk/gov/hmrc/jenkinsjobs/domain/variable/JavaVersionEnvironmentVariable.groovy
@@ -5,7 +5,7 @@ import uk.gov.hmrc.jenkinsjobbuilders.domain.variable.EnvironmentVariable
 import static uk.gov.hmrc.jenkinsjobbuilders.domain.variable.StringEnvironmentVariable.stringEnvironmentVariable
 
 enum JavaVersionEnvironmentVariable implements EnvironmentVariable {
-    JDKLATEST('1.8.0_112')
+    JDKLATEST('1.8.0_101')
 
     private final EnvironmentVariable environmentVariable
 
