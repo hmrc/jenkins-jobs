d9e5cb64082b8308c4762fda6c5427c1ac00f940
adam-coles <adam.coles@digital.hmrc.gov.uk>
2016-11-07 11:42:31 +0000

    added amls to config

--- jobs/ci_open_jenkins/build/amls.groovy
+++ jobs/ci_open_jenkins/build/amls.groovy
@@ -1,7 +1,16 @@
 package ci_open_jenkins.build
 
 import javaposse.jobdsl.dsl.DslFactory
+import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.BuildMonitorViewBuilder
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtLibraryJobBuilder
 
 new SbtLibraryJobBuilder('play-whitelist-filter').
         build(this as DslFactory)
+
+new SbtLibraryJobBuilder('amls').
+        build(this as DslFactory)
+
+new BuildMonitorViewBuilder('AMLS-MONITOR')
+        .withJobs('play-whitelist-filter',
+        'amls'
+).build(this)
