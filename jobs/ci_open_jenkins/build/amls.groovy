5879ae01dea9a19e616842ad3bd113c77a2a3787
adam-coles <adam.coles@digital.hmrc.gov.uk>
2016-11-07 15:33:17 +0000

    fixed job type for amls

--- jobs/ci_open_jenkins/build/amls.groovy
+++ jobs/ci_open_jenkins/build/amls.groovy
@@ -3,11 +3,12 @@ package ci_open_jenkins.build
 import javaposse.jobdsl.dsl.DslFactory
 import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.BuildMonitorViewBuilder
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtLibraryJobBuilder
+import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtMicroserviceJobBuilder
 
 new SbtLibraryJobBuilder('play-whitelist-filter').
         build(this as DslFactory)
 
-new SbtLibraryJobBuilder('amls').
+new SbtMicroserviceJobBuilder('amls').
         build(this as DslFactory)
 
 new BuildMonitorViewBuilder('AMLS-MONITOR')
