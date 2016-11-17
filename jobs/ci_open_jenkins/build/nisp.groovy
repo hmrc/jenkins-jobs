8bd099fc6b3bcbe0b28dda82f9bd99b3647aaefe
Callum Turnbull <callum@turnbull.codes>
2016-11-17 11:26:43 +0000

    Add moar NISP Hotfix

--- jobs/ci_open_jenkins/build/nisp.groovy
+++ jobs/ci_open_jenkins/build/nisp.groovy
@@ -15,7 +15,10 @@ new SbtFrontendJobBuilder('nisp-frontend').
         withSCoverage().
         build(this as DslFactory)
 
-new SbtFrontendJobBuilder('nisp-frontend-hotfix', 'nisp-frontend', 'iv-fix')
+new SbtFrontendJobBuilder('nisp-frontend-hotfix', 'nisp-frontend', 'iv-fix').
+	withScalaStyle().
+	withSCoverage().
+	build(this as DslFactory)
 
 new SbtMicroserviceJobBuilder('state-pension').
         withScalaStyle().
