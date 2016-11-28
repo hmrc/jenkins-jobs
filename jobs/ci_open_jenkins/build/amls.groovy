cea0809839b305aec0fd2dbd957bd5eae912b26c
adam-coles <adam.coles@digital.hmrc.gov.uk>
2016-11-28 11:57:57 +0000

    updated amls with scoverage and scalastyle

--- jobs/ci_open_jenkins/build/amls.groovy
+++ jobs/ci_open_jenkins/build/amls.groovy
@@ -6,12 +6,17 @@ import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtLibraryJobBuilder
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtMicroserviceJobBuilder
 
 new SbtLibraryJobBuilder('play-whitelist-filter').
+        withSCoverage().
         build(this as DslFactory)
 
 new SbtMicroserviceJobBuilder('amls').
+        withScalaStyle().
+        withSCoverage().
         build(this as DslFactory)
 
 new SbtMicroserviceJobBuilder('amls-notification').
+        withScalaStyle().
+        withSCoverage().
         build(this as DslFactory)
 
 new BuildMonitorViewBuilder('AMLS-MONITOR')
