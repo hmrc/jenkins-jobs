2f0e59e0ef4e8da8f85688f0c797595817e51655
nikhil Bharti <nikhil.bharti@digital.hmrc.gov.uk>
2016-12-01 16:18:09 +0000

    Added awrs-lookup service

--- jobs/ci_open_jenkins/build/awrs.groovy
+++ jobs/ci_open_jenkins/build/awrs.groovy
@@ -3,6 +3,7 @@ package ci_open_jenkins.build
 import javaposse.jobdsl.dsl.DslFactory
 import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.BuildMonitorViewBuilder
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtMicroserviceJobBuilder
+import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtFrontendJobBuilder
 
 new SbtMicroserviceJobBuilder('api-integration-test-runner').
         build(this as DslFactory)
@@ -19,5 +20,16 @@ new SbtMicroserviceJobBuilder('awrs').
         withTests("test").
         build(this as DslFactory)
 
+new SbtMicroserviceJobBuilder('awrs-lookup').
+        withScalaStyle().
+        withSCoverage().
+        withTests("test").
+        build(this as DslFactory)
+
+new SbtFrontendJobBuilder('awrs-lookup-frontend').
+        withScalaStyle().
+        withSCoverage().
+        build(this as DslFactory)
+
 new BuildMonitorViewBuilder('AWRS-MONITOR')
-        .withJobs('awrs','awrs-notification','api-integration-test-runner').build(this)
+        .withJobs('awrs','awrs-notification','api-integration-test-runner','awrs-lookup','awrs-lookup-frontend').build(this)
