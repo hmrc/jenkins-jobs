577ae1c629479f2bb90261bd832c98b1393a7171
Thomas VANDEVELDE <thomas.vandevelde@digital.hmrc.gov.uk>
2016-12-05 16:05:04 +0000

    API-2196 - Add api-platform-test-login-frontend

--- jobs/ci_open_jenkins/build/api.groovy
+++ jobs/ci_open_jenkins/build/api.groovy
@@ -30,4 +30,4 @@ new SbtMicroserviceJobBuilder('api-platform-test-user').
         build(this as DslFactory)
 
 new BuildMonitorViewBuilder('API-MONITOR')
-        .withJobs('api-example-scala-client', 'api-revocation-frontend', 'play-json-union-formatter', 'api-platform-test-user', 'api-platform-test-user-frontend').build(this)
+        .withJobs('api-example-scala-client', 'api-revocation-frontend', 'play-json-union-formatter', 'api-platform-test-user', 'api-platform-test-user-frontend', 'api-platform-test-login-frontend').build(this)
