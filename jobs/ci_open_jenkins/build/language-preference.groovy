68bd3f3864d18447030c7e18670361e04e53c5fc
Raghu Adimulam <adimulam.raghu@gmail.com>
2016-12-06 16:03:38 +0000

    Update language-preference.groovy

--- jobs/ci_open_jenkins/build/language-preference.groovy
+++ jobs/ci_open_jenkins/build/language-preference.groovy
@@ -3,7 +3,6 @@ package ci_open_jenkins.build
 import javaposse.jobdsl.dsl.DslFactory
 import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.BuildMonitorViewBuilder
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtFrontendJobBuilder
-import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtMicroserviceJobBuilder
 
 new SbtFrontendJobBuilder('language-preference').
         withScalaStyle().
