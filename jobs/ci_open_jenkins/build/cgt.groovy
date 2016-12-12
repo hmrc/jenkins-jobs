d2beca7ed7066ca1940da0d5894b6d5587eeaa80
david.woodward <david.woodward@digital.hmrc.gov.uk>
2016-12-12 14:43:51 +0000

    Added the shares jenkins config.

--- jobs/ci_open_jenkins/build/cgt.groovy
+++ jobs/ci_open_jenkins/build/cgt.groovy
@@ -22,6 +22,13 @@ new SbtMicroserviceJobBuilder('cgt-calculator-non-resident-frontend').
         withExtendedTimeout().
         build(this as DslFactory)
 
+
+new SbtMicroserviceJobBuilder('cgt-calculator-resident-shares-frontend').
+        withScalaStyle().
+        withSCoverage().
+        withExtendedTimeout().
+        build(this as DslFactory)
+
 new SbtMicroserviceJobBuilder('capital-gains-subscription').
         withScalaStyle().
         withSCoverage().
@@ -33,4 +40,4 @@ new SbtFrontendJobBuilder('capital-gains-subscription-frontend').
         build(this as DslFactory)
 
 new BuildMonitorViewBuilder('CGT-MONITOR')
-        .withJobs('capital-gains-calculator-frontend','capital-gains-calculator','capital-gains-subscription', 'capital-gains-subscription-frontend', 'cgt-calculator-non-resident-frontend').build(this)
+        .withJobs('capital-gains-calculator-frontend','capital-gains-calculator','capital-gains-subscription', 'capital-gains-subscription-frontend', 'cgt-calculator-non-resident-frontend', 'cgt-calculator-resident-shares-frontend').build(this)
