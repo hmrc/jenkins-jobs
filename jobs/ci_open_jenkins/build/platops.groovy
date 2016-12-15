b3286144d0d55567d5744cd9a146ccd2ec2c34d0
thomas.vogel <thomas.vogel@digital.hmrc.gov.uk>
2016-12-15 15:16:35 +0000

    [PLATOPS-712] Added a create-a-service Jenkins job.

--- jobs/ci_open_jenkins/build/platops.groovy
+++ jobs/ci_open_jenkins/build/platops.groovy
@@ -20,24 +20,24 @@ import static uk.gov.hmrc.jenkinsjobs.domain.publisher.Publishers.*
 import static uk.gov.hmrc.jenkinsjobs.domain.step.Steps.*
 
 new SbtLibraryJobBuilder('sbt-git-versioning').
-                         withoutJUnitReports().
-                         build(this)
+        withoutJUnitReports().
+        build(this)
 
 new SbtLibraryJobBuilder('time').
-                         build(this)
+        build(this)
 
 new SbtLibraryJobBuilder('sbt-bobby').
-                         build(this)
+        build(this)
 
 new GradleLibraryJobBuilder('jenkins-job-builders').
-                            build(this)
+        build(this)
 
 new SbtLibraryJobBuilder('git-stamp').
-                         withoutJUnitReports().
-                         build(this)
+        withoutJUnitReports().
+        build(this)
 
 new SbtLibraryJobBuilder('init-repository').
-                        build(this)
+        build(this)
 
 
 jobBuilder('init-service', 'init-service', 'master')
@@ -53,31 +53,31 @@ new SbtLibraryJobBuilder('init-webhook').build(this)
 
 
 new SbtLibraryJobBuilder('releaser').
-                         build(this)
+        build(this)
 
 new SbtLibraryJobBuilder('govuk-template').
-                         withoutJUnitReports().
-                         build(this)
+        withoutJUnitReports().
+        build(this)
 
 new SbtLibraryJobBuilder('sbt-bintray-publish').
-                         withoutJUnitReports().
-                         build(this)
+        withoutJUnitReports().
+        build(this)
 
 new SbtLibraryJobBuilder('sbt-auto-build').
-                         withoutJUnitReports().
-                         build(this)
+        withoutJUnitReports().
+        build(this)
 
 new SbtLibraryJobBuilder('sbt-git-stamp').
-                         withoutJUnitReports().
-                         build(this)
+        withoutJUnitReports().
+        build(this)
 
 new SbtLibraryJobBuilder('sbt-settings').
-                         withoutJUnitReports().
-                         build(this)
+        withoutJUnitReports().
+        build(this)
 
 new SbtLibraryJobBuilder('sbt-distributables').
-                         withoutJUnitReports().
-                         build(this)
+        withoutJUnitReports().
+        build(this)
 
 jobBuilder("ReactiveMongo-HMRC-Fork", "ReactiveMongo").
         withSteps(cleanPublishSigned()).
@@ -86,15 +86,15 @@ jobBuilder("ReactiveMongo-HMRC-Fork", "ReactiveMongo").
         build(this)
 
 jobBuilder('create-a-repository').
-          withEnvironmentVariables(stringEnvironmentVariable('INIT_REPO_VERSION', '0.23.0')).
-          withParameters(stringParameter('REPOSITORY_NAME','','The repository name e.g. foo-frontend')).
-          withParameters(stringParameter('TEAM_NAME','','The exact name of the github team to which the repository will be added')).
-          withParameters(choiceParameter('REPOSITORY_TYPE',['Sbt','SbtPlugin'],'The repository type e.g. SBT')).
-          withParameters(stringParameter('BOOTSTRAP_TAG','0.1.0','The bootstrap tag to kickstart release candidates. This should be 0.1.0 for *new* repositories or the most recent internal tag version for *migrated* repositories')).
-          //withParameters(booleanParameter('ENABLE_TRAVIS',false,'enable travis intigration')).
-          withSteps(createARepository('$REPOSITORY_NAME', '$TEAM_NAME', '$REPOSITORY_TYPE', '$BOOTSTRAP_TAG', '$ENABLE_TRAVIS')).
-          withPublishers(buildDescriptionByRegexPublisher('\\[INFO\\] Github repositories and Bintray packages successfully created (.*)')).
-          build(this)
+        withEnvironmentVariables(stringEnvironmentVariable('INIT_REPO_VERSION', '0.23.0')).
+        withParameters(stringParameter('REPOSITORY_NAME', '', 'The repository name e.g. foo-frontend')).
+        withParameters(stringParameter('TEAM_NAME', '', 'The exact name of the github team to which the repository will be added')).
+        withParameters(choiceParameter('REPOSITORY_TYPE', ['Sbt', 'SbtPlugin'], 'The repository type e.g. SBT')).
+        withParameters(stringParameter('BOOTSTRAP_TAG', '0.1.0', 'The bootstrap tag to kickstart release candidates. This should be 0.1.0 for *new* repositories or the most recent internal tag version for *migrated* repositories')).
+//withParameters(booleanParameter('ENABLE_TRAVIS',false,'enable travis intigration')).
+        withSteps(createARepository('$REPOSITORY_NAME', '$TEAM_NAME', '$REPOSITORY_TYPE', '$BOOTSTRAP_TAG', '$ENABLE_TRAVIS')).
+        withPublishers(buildDescriptionByRegexPublisher('\\[INFO\\] Github repositories and Bintray packages successfully created (.*)')).
+        build(this)
 
 
 jobBuilder('create-a-webhook')
@@ -104,36 +104,38 @@ jobBuilder('create-a-webhook')
         .withParameters(stringParameter('ORG', 'hmrc', 'repository organization'))
         .withParameters(stringParameter('REPOSITORY_NAMES', '', 'comma separated list of repository names e.g. foo-frontend,foo-service'))
         .withParameters(stringParameter('WEBHOOK_URL', '', 'url for the notification'))
-        .withParameters(stringParameter('EVENTS', 'issues,issue_comment,pull_request,pull_request_review_comment','comma separated list of git events to be notified e.g issues,pull_request if not specified defaults will be used'))
+        .withParameters(stringParameter('EVENTS', 'issues,issue_comment,pull_request,pull_request_review_comment', 'comma separated list of git events to be notified e.g issues,pull_request if not specified defaults will be used'))
         .withSteps(createAWebhook('$CRED_FILE_PATH', '$API_BASE_URL', '$ORG', '$REPOSITORY_NAMES', '$WEBHOOK_URL', '$EVENTS'))
         .build(this)
 
 
 jobBuilder('create-a-release').
-           withEnvironmentVariables(stringEnvironmentVariable('RELEASER_VERSION', '1.0.0')).
-           withParameters(stringParameter('ARTEFACT_NAME','','The artifact name e.g. cato-frontend'),
-                          stringParameter('RELEASE_CANDIDATE_VERSION', '', 'The release candidate e.g. 1.3.0-1-g21312cc'),
-                          choiceParameter('RELEASE_TYPE', ['MINOR', 'MAJOR', 'HOTFIX'], 'The type of release e.g. MINOR')).
-           withSteps(createARelease()).
-           withPublishers(buildDescriptionByRegexPublisher('\\[INFO\\] Releaser successfully released (.*)')).
-           build(this)
+        withEnvironmentVariables(stringEnvironmentVariable('RELEASER_VERSION', '1.0.0')).
+        withParameters(stringParameter('ARTEFACT_NAME', '', 'The artifact name e.g. cato-frontend'),
+                stringParameter('RELEASE_CANDIDATE_VERSION', '', 'The release candidate e.g. 1.3.0-1-g21312cc'),
+                choiceParameter('RELEASE_TYPE', ['MINOR', 'MAJOR', 'HOTFIX'], 'The type of release e.g. MINOR')).
+        withSteps(createARelease()).
+        withPublishers(buildDescriptionByRegexPublisher('\\[INFO\\] Releaser successfully released (.*)')).
+        build(this)
 
 jobBuilder('clean-slaves').
-           withParameters(nodeParameter('slaves', (1..4).collect {"ci-open-slave-$it"}, 'allowMultiSelectionForConcurrentBuilds')).
-           withConcurrentBuilds().
-           withLabel('master').
-           withTriggers(cronTrigger('H 23 * * 4')).
-           withSteps(shellStep("""\
+        withParameters(nodeParameter('slaves', (1..4).collect {
+            "ci-open-slave-$it"
+        }, 'allowMultiSelectionForConcurrentBuilds')).
+        withConcurrentBuilds().
+        withLabel('master').
+        withTriggers(cronTrigger('H 23 * * 4')).
+        withSteps(shellStep("""\
                                |rm -rf ~/.m2/repository
                                |rm -rf ~/.ivy2
                                |find ~/workspace -maxdepth 1 -type d -mtime +30 -print0 | xargs -0 rm -rf
                                |find /var/tmp -maxdepth 1 -type d -name "*_LOCAL" -user "jenkins" -print0 | xargs -0 rm -rf
                                """.stripMargin())).
-           build(this)
+        build(this)
 
 
 new SbtMicroserviceJobBuilder('teams-and-repositories').withTests("test")
-       .build(this as DslFactory)
+        .build(this as DslFactory)
 
 new SbtMicroserviceJobBuilder('catalogue-frontend').withTests("test")
         .build(this as DslFactory)
@@ -155,3 +157,22 @@ new SbtLibraryJobBuilder('git-client').build(this as DslFactory)
 
 new BuildMonitorViewBuilder('PLATOPS-MONITOR')
         .withJobs('sbt-git-versioning', 'time', 'sbt-bobby', 'jenkins-job-builders', 'git-stamp', 'init-repository', 'releaser', 'govuk-template', 'sbt-bintray-publish', 'sbt-auto-build', 'sbt-git-stamp', 'sbt-settings', 'sbt-distributables', 'teams-and-services', 'catalogue-frontend', 'alert-config-builder', 'init-service', 'indicators', 'service-deployments', 'create-a-release', 'create-a-repository', 'create-a-webhook', 'github-client').build(this)
+
+jobBuilder('create-a-service', HmrcGitHubComScm('init-service'), "Creates a Scala Play service")
+        .withEnvironmentVariables(stringEnvironmentVariable('INIT_REPOSITORY_VERSION', '0.23.0'))
+        .withParameters(stringParameter('REPOSITORY_NAME', '', 'The repository name e.g. foo-frontend. See <a href="https://confluence.tools.tax.service.gov.uk/display/AR/Microservice+Naming+Guidelines">Naming Guidelines</a>'))
+        .withParameters(stringParameter('TEAM_NAME', '', 'The exact name of the team as in: <a href="https://catalogue.tax.service.gov.uk/teams">catalogue</a>'))
+        .withParameters(choiceParameter('TYPE', ['MICROSERVICE', 'FRONTEND'], 'Type of the service'))
+        .withParameters(booleanParameter('WITH_MONGO', false, 'Selecting this options will include the mongo connector library and plugin as project dependencies.'))
+        .withSteps(createARepository('$REPOSITORY_NAME', '$TEAM_NAME', '$REPOSITORY_TYPE', '$BOOTSTRAP_TAG', '$ENABLE_TRAVIS'))
+        .withSteps(shellStep("""|
+                                |withMongo=""
+                                |
+                                |if [ "\$WITH_MONGO" = true ]; then
+                                |  withMongo="-use_mongo"
+                                |fi
+                                |
+                                |python scripts/bin/create.py \${REPOSITORY_NAME} \${TYPE} -exists \$withMongo
+                               """.stripMargin()))
+        .withPublishers(buildDescriptionByRegexPublisher('\\[INFO\\] Successfully created (.*)'))
+        .build(this)
