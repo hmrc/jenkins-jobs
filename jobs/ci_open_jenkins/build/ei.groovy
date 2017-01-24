bb2fc4e6708ae3a96028dc257aa6ee46fe19bc24
Rambabu Posa <rambabu.posa@digital.hmrc.gov.uk>
2017-01-24 12:10:49 +0000

    DDCNLS-583: Transferred EI Jenkins Jobs to LS Team.

--- jobs/ci_open_jenkins/build/ei.groovy
+++
@@ -1,7 +0,0 @@
-package ci_open_jenkins.build
-
-import javaposse.jobdsl.dsl.DslFactory
-import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtLibraryJobBuilder
-
-new SbtLibraryJobBuilder('tabular-data-validator').
-        build(this as DslFactory)
