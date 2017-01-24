23ff662f78c8bd4a1723ce54bc343f728f239af6
Ralf Oenning <ralf@invicto.net>
2017-01-24 11:26:44 +0000

    build service job for bforms

---
+++ jobs/ci_open_jenkins/build/bforms.groovy
@@ -0,0 +1,9 @@
+package ci_open_jenkins.build
+
+import javaposse.jobdsl.dsl.DslFactory
+import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.BuildMonitorViewBuilder
+import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtMicroserviceJobBuilder
+import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtFrontendJobBuilder
+
+new SbtFrontendJobBuilder('bforms-frontend').
+        build(this as DslFactory)
