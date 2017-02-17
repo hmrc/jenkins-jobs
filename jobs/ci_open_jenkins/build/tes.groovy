d3f133b9c0033c8ed9b36ece4cf6f1f85b592775
Duncan Crawford <duncan@noughtsandones.co.uk>
2017-02-17 14:03:31 +0000

    add personal-income job to tes dashboard
    personal-income is a consumer of personal-tax-summary and therefore should be unaffected by changes personal-tax-summary but if it is then TES show be made aware of this

--- jobs/ci_open_jenkins/build/tes.groovy
+++ jobs/ci_open_jenkins/build/tes.groovy
@@ -4,9 +4,12 @@ import javaposse.jobdsl.dsl.DslFactory
 import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.BuildMonitorViewBuilder
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtFrontendJobBuilder
 
+new SbtFrontendJobBuilder('personal-income').build(this as DslFactory)
+
 new SbtFrontendJobBuilder('personal-tax-summary').build(this as DslFactory)
 
 new BuildMonitorViewBuilder('TES-MONITOR')
-		.withJobs('personal-tax-summary').build(this)
+		.withJobs('personal-income',
+		'personal-tax-summary').build(this)
 
 
