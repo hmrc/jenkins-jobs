c994a1f95c1c3a54a715ef31b3ecbecb63254877
Chris Waugh <cbwaugh@gmail.com>
2017-03-03 12:04:20 +0000

    Updated lisa.groovy
    Added reference to frontendjobbuilder

--- jobs/ci_open_jenkins/build/lisa.groovy
+++ jobs/ci_open_jenkins/build/lisa.groovy
@@ -3,6 +3,7 @@ package ci_open_jenkins.build
 import javaposse.jobdsl.dsl.DslFactory
 import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.BuildMonitorViewBuilder
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtMicroserviceJobBuilder
+import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtFrontendJobBuilder
 
 new SbtMicroserviceJobBuilder('lisa-api').
         withTests("test").
