3a1db4741f551f2539588b3305dcef0617eaf01d
stephen.ward <stephen.ward@digital.hmrc.gov.uk>
2017-03-09 11:30:24 +0000

    Amended scrs.groovy for new incorporation-information mservice

--- jobs/ci_open_jenkins/build/scrs.groovy
+++ jobs/ci_open_jenkins/build/scrs.groovy
@@ -26,6 +26,7 @@ new SbtMicroserviceJobBuilder('company-registration').
         withSCoverage().
         build(this as DslFactory)
 
+<<<<<<< HEAD
 new SbtFrontendJobBuilder('company-registration-scrs-5444-hotfix', 'company-registration', 'SCRS-5444').
         build(this as DslFactory)
 
@@ -51,3 +52,12 @@ new SbtMicroserviceJobBuilder('vat-registration').
 
 new BuildMonitorViewBuilder('SCRS-MONITOR')
         .withJobs('business-registration-frontend', 'incorporation-dynamic-stub', 'business-registration', 'company-registration', 'business-registration-notification', 'business-registration-dynamic-stub', 'vat-registration-frontend', 'vat-registration').build(this)
+=======
+new SbtMicroserviceJobBuilder('incorporation-information').
+        withScalaStyle().
+        withSCoverage().
+        build(this as DslFactory)        
+
+new BuildMonitorViewBuilder('SCRS-MONITOR')
+.withJobs('business-registration-frontend', 'incorporation-dynamic-stub', 'business-registration', 'company-registration', 'incorporation-information').build(this)
+>>>>>>> Amended scrs.groovy for new incorporation-information mservice
