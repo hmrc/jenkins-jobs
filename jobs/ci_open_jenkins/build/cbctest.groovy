2138c31a5f4229fcfba5d8dea36f75229de6971f
Ralf Oenning <ralreiroe@users.noreply.github.com>
2017-03-13 18:50:11 +0000

    Create cbctest.groovy

---
+++ jobs/ci_open_jenkins/build/cbctest.groovy
@@ -0,0 +1,12 @@
+package ci_open_jenkins.build
+
+import javaposse.jobdsl.dsl.DslFactory
+import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.BuildMonitorViewBuilder
+import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtMicroserviceJobBuilder
+import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtFrontendJobBuilder
+
+new SbtFrontendJobBuilder('cbc-frontend').
+        build(this as DslFactory)
+
+new SbtMicroserviceJobBuilder('cbc').
+        build(this as DslFactory)
