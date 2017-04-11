29a17afe770b74dcf594cfba98ee3cb315314781
Nick Karaolis <nick.karaolis@digital.hmrc.gov.uk>
2017-04-11 13:34:55 +0100

    Optimised imports and removed unused imports

--- jobs/ci_open_jenkins/build/sdil.groovy
+++ jobs/ci_open_jenkins/build/sdil.groovy
@@ -3,15 +3,7 @@ package ci_open_jenkins.build
 import javaposse.jobdsl.dsl.DslFactory
 import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.BuildMonitorViewBuilder
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtFrontendJobBuilder
-import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtLibraryJobBuilder
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtMicroserviceJobBuilder
-
-import static uk.gov.hmrc.jenkinsjobbuilders.domain.parameters.ChoiceParameter.choiceParameter
-import static uk.gov.hmrc.jenkinsjobbuilders.domain.parameters.StringParameter.stringParameter
-import static uk.gov.hmrc.jenkinsjobbuilders.domain.publisher.BuildDescriptionPublisher.buildDescriptionByRegexPublisher
-import static uk.gov.hmrc.jenkinsjobbuilders.domain.variable.StringEnvironmentVariable.stringEnvironmentVariable
-import static uk.gov.hmrc.jenkinsjobs.domain.builder.JobBuilders.jobBuilder
-import static uk.gov.hmrc.jenkinsjobs.domain.step.Steps.createAJavaRelease
 
 def frontends = ["soft-drinks-industry-levy-frontend"]
 
