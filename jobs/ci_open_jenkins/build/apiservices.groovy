a8a4b55314215d3c3f4193a29252ba05d6f7bab4
Stefano Sgorlon <gokyo@users.noreply.github.com>
2017-04-11 15:02:30 +0100

    API-2411: fixing compilation error (forgot `import`)

--- jobs/ci_open_jenkins/build/apiservices.groovy
+++ jobs/ci_open_jenkins/build/apiservices.groovy
@@ -2,6 +2,7 @@ package ci_open_jenkins.build
 import javaposse.jobdsl.dsl.DslFactory
 import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.BuildMonitorViewBuilder
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtFrontendJobBuilder
+import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtLibraryJobBuilder
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtMicroserviceJobBuilder
 
 new SbtFrontendJobBuilder('api-example-scala-client').
