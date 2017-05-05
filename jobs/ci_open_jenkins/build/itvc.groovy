e206638e991526fec58e62c4f9fd7e3087000650
MarkAKelly <mark.kelly@digital.hmrc.gov.uk>
2017-05-05 14:08:30 +0100

    [BTAT-28] add income tax view change frontend

---
+++ jobs/ci_open_jenkins/build/itvc.groovy
@@ -0,0 +1,15 @@
+package ci_open_jenkins.build
+
+import javaposse.jobdsl.dsl.DslFactory
+import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.BuildMonitorViewBuilder
+import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtMicroserviceJobBuilder
+import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtFrontendJobBuilder
+
+new SbtFrontendJobBuilder('income-tax-view-change-frontend').
+        withScalaStyle().
+        withSCoverage().
+        build(this as DslFactory)
+
+new BuildMonitorViewBuilder('ITVC-MONITOR').withJobs(
+        'income-tax-view-change-frontend'
+).build(this)
