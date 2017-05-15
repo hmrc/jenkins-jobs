6587cc36ddd73a432315db253140cade88950be7
Luke Smith <lukesmudgesmith@gmail.com>
2017-05-15 12:55:25 +0100

    Correct service name

--- jobs/ci_open_jenkins/build/psal.groovy
+++ jobs/ci_open_jenkins/build/psal.groovy
@@ -4,7 +4,7 @@ import javaposse.jobdsl.dsl.DslFactory
 import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.BuildMonitorViewBuilder
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtMicroserviceJobBuilder
 
-def services = ["pensions-scheme-administrator-lookup-api"]
+def services = ["pension-scheme-administrator-lookup-api"]
 
 services.each {
     new SbtMicroserviceJobBuilder(it)
