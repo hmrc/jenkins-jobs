2073df660d0b736b33f67d294ad8801b39733aa9
Mohammed Abdulrazeg <github@mzawi.me>
2017-05-18 19:07:09 +0100

    Added microservice builder and jenkins view

--- jobs/ci_open_jenkins/build/remote-template.groovy
+++ jobs/ci_open_jenkins/build/remote-template.groovy
@@ -5,3 +5,11 @@ import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtLibraryJobBuilder
 
 new SbtLibraryJobBuilder('local-template-renderer').
         build(this as DslFactory)
+
+new SbtMicroserviceJobBuilder('frontend-template-provider').
+        build(this as DslFactory)
+
+new BuildMonitorViewBuilder('TAAS-MONITOR')
+        .withJobs('local-template-renderer',
+                  'frontend-template-provider')
+        .build(this)
