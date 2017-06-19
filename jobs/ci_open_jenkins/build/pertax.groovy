17c5fd297cd4ad1a7a40dd27dc19192acb4ef319
Nic Fellows <nick.fellows@accenture.com>
2017-06-19 13:09:11 +0100

    Add pertax-frontend jobs

--- jobs/ci_open_jenkins/build/pertax.groovy
+++ jobs/ci_open_jenkins/build/pertax.groovy
@@ -2,6 +2,17 @@ package ci_open_jenkins.build
 
 import javaposse.jobdsl.dsl.DslFactory
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtLibraryJobBuilder
+import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.BuildMonitorViewBuilder
+import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtFrontendJobBuilder
 
 new SbtLibraryJobBuilder('accessibility-driver').
         build(this as DslFactory)
+
+new SbtFrontendJobBuilder('pertax-frontend').
+		withScalaStyle().
+		withSCoverage().
+		build(this as DslFactory)
+
+new BuildMonitorViewBuilder('PERTAX-MONITOR')
+        .withJobs('pertax-frontend').build(this)
+
