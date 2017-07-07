da0e1305e5a3365aca02f58a83a4a8faffc93991
Alen Mujezinovic <alen@owns.ch>
2017-07-07 14:36:26 +0100

    Revert "Use job builder with custom shellStep for help-frontend"
    This reverts commit 94c608b2661b84942cee71ab83ff6e8392269f2b.

--- jobs/ci_open_jenkins/build/bta.groovy
+++ jobs/ci_open_jenkins/build/bta.groovy
@@ -1,18 +1,13 @@
 package ci_open_jenkins.build
 
-
 import javaposse.jobdsl.dsl.DslFactory
 import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.BuildMonitorViewBuilder
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtFrontendJobBuilder
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtLibraryJobBuilder
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtMicroserviceJobBuilder
-import static uk.gov.hmrc.jenkinsjobs.domain.builder.JobBuilders.jobBuilder
-import static uk.gov.hmrc.jenkinsjobbuilders.domain.step.ShellStep.shellStep
 
-jobBuilder('help-frontend').
-        withSteps(shellStep('./jenkins-help-frontend.sh')).
-        build(this)
-
+new SbtFrontendJobBuilder('help-frontend').
+                         build(this as DslFactory)
 
 new SbtLibraryJobBuilder('frontend-bootstrap').
         build(this as DslFactory)
