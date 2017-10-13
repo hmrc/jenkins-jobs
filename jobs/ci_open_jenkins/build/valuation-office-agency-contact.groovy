160ea2dc1cc49c623dffc0bfb8db103cf865804d
Andy Dwelly <andy.dwelly@digital.hmrc.gov.uk>
2017-10-13 14:05:15 +0100

    valuation-office-agency-contact: Initial implementation.

---
+++ jobs/ci_open_jenkins/build/valuation-office-agency-contact.groovy
@@ -0,0 +1,21 @@
+package ci_open_jenkins.build
+
+import javaposse.jobdsl.dsl.DslFactory
+import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.BuildMonitorViewBuilder
+import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtFrontendJobBuilder
+import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtMicroserviceJobBuilder
+
+new SbtMicroserviceJobBuilder('lightweight-contact-events').
+        withScalaStyle().
+        build(this as DslFactory)
+
+new SbtFrontendJobBuilder('valuation-office-agency-contact-frontend').
+        withScalaStyle().
+        withSCoverage().
+        build(this as DslFactory)
+
+// TODO: Add acceptance test job
+
+new BuildMonitorViewBuilder('VOAC-CONTACT')
+        .withJobs('lightweight-contact-events',
+        'valuation-office-agency-contact-frontend').build(this)
