97016dda3a7d2cd84dc6dce937abb7ffbbb5e96e
Oli Slade <oliverslade@users.noreply.github.com>
2017-11-17 13:37:14 +0000

    Create claim-tax-refund.groovy

---
+++ jobs/ci_open_jenkins/build/claim-tax-refund.groovy
@@ -0,0 +1,23 @@
+package ci_open_jenkins.build
+
+import javaposse.jobdsl.dsl.DslFactory
+import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.BuildMonitorViewBuilder
+import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtFrontendJobBuilder
+
+new ListViewBuilder('CTR')
+        .withJobsRegex('claim-tax-refund.*')
+        .build(this as DslFactory)
+
+new BuildMonitorViewBuilder('CTR-Monitor')
+        .withJobs(
+        'claim-tax-refund-frontend',
+        'claim-tax-refund-acceptance-tests')
+        .build(this as DslFactory)
+
+new SbtFrontendJobBuilder('claim-tax-refund-frontend', 'hmrc/claim-tax-refund-frontend').
+        withXvfb().
+        withDeployToDev().
+        withStaticAnalysisAndReport().
+        withDownstreamJobs('claim-tax-refund-acceptance-tests').
+        build(this as DslFactory)
+        
