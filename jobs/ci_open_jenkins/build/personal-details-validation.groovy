59e3a2818816a062c79e8134ece8a52c37a7d7ef
Jakub Chrobasik <jakub.chrobasik@gmail.com>
2017-11-22 11:22:16 +0000

    TSV-747 Added build job for personal-details-validation service

---
+++ jobs/ci_open_jenkins/build/personal-details-validation.groovy
@@ -0,0 +1,8 @@
+package ci_open_jenkins.build
+
+import javaposse.jobdsl.dsl.DslFactory
+import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtFrontendJobBuilder
+import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtMicroserviceJobBuilder
+
+new SbtMicroserviceJobBuilder('personal-details-validation').
+    build(this as DslFactory)
