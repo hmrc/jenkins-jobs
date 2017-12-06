b485fbdf40dd2b301c9c4e32e4a0c10355042b9c
eisenhower444 <tomvogel01@gmail.com>
2017-12-06 10:49:20 +0000

    Separated the jenkins jobs belonging to epaye from bta

---
+++ jobs/ci_open_jenkins/build/epaye.groovy
@@ -0,0 +1,18 @@
+package ci_open_jenkins.build
+
+import javaposse.jobdsl.dsl.DslFactory
+import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.BuildMonitorViewBuilder
+import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtFrontendJobBuilder
+import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtLibraryJobBuilder
+import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtMicroserviceJobBuilder
+
+new SbtMicroserviceJobBuilder('epaye-api').
+        withDownstreamJobs('epaye-frontend-acceptance-tests').
+        withTests("test").
+        withScalaStyle().
+        withSCoverage().
+        build(this as DslFactory)
+
+new BuildMonitorViewBuilder('EPAYE-OPEN-DEV-MONITOR')
+        .withJobs('epaye-api')
+        .build(this)
