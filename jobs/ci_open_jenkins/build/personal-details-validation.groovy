01249b822bc7270d58c7b181c9c30b81298b1057
Anshul Bajpai <anshul.bajpai@digital.hmrc.gov.uk>
2017-12-11 14:58:16 +0000

    TSV-754 Corrected job for personal-details-validation-frontend build

--- jobs/ci_open_jenkins/build/personal-details-validation.groovy
+++ jobs/ci_open_jenkins/build/personal-details-validation.groovy
@@ -7,5 +7,6 @@ import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtMicroserviceJobBuilder
 new SbtMicroserviceJobBuilder('personal-details-validation').
     build(this as DslFactory)
 
-new SbtMicroserviceJobBuilder('personal-details-validation-frontend').
+new SbtFrontendJobBuilder('personal-details-validation-frontend').
+        withXvfb().
         build(this as DslFactory)
