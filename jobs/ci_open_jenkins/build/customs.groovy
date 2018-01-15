4380a8007cb271dab13cacf8b944755c2e670dac
mihails-cirganovics <mihails.cirganovics@digital.hmrc.gov.uk>
2018-01-15 10:20:06 +0000

    CDD-1234: build job for customs-declarations

--- jobs/ci_open_jenkins/build/customs.groovy
+++ jobs/ci_open_jenkins/build/customs.groovy
@@ -5,12 +5,18 @@ import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtMicroserviceJobBuilder
 import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.BuildMonitorViewBuilder
 
 def customsInventoryLinkingImports = 'customs-inventory-linking-imports'
+def customsDeclarations = 'customs-declarations'
 def customsApiCommon = 'customs-api-common'
 
 new SbtMicroserviceJobBuilder(customsInventoryLinkingImports).
     withSCoverage().
     withScalaStyle().
     build(this as DslFactory)
+
+new SbtMicroserviceJobBuilder(customsDeclarations).
+        withSCoverage().
+        withScalaStyle().
+        build(this as DslFactory)
 
 new SbtMicroserviceJobBuilder(customsApiCommon).
     withSCoverage().
@@ -20,5 +26,6 @@ new SbtMicroserviceJobBuilder(customsApiCommon).
 new BuildMonitorViewBuilder('CUSTOMS-MONITOR').
     withJobs(
         customsInventoryLinkingImports,
+        customsDeclarations,
         customsApiCommon).
     build(this)
