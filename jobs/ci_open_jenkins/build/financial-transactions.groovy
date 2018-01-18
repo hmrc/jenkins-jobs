af687595ec52dc1bf173b27eec0630dff35b332f
matthew.page <matthew.page@digital.hmrc.gov.uk>
2018-01-18 12:00:41 +0000

    [BTAT-1593] Added Financial Transactions microservice job builder

---
+++ jobs/ci_open_jenkins/build/financial-transactions.groovy
@@ -0,0 +1,12 @@
+package ci_open_jenkins.build
+
+import javaposse.jobdsl.dsl.DslFactory
+import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.BuildMonitorViewBuilder
+import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtMicroserviceJobBuilder
+
+new SbtMicroserviceJobBuilder('financial-transactions')
+        .withScalaStyle()
+        .withSCoverage()
+		    .build(this as DslFactory)
+
+new BuildMonitorViewBuilder('FT-MONITOR').withJobs('financial-transactions').build(this)
