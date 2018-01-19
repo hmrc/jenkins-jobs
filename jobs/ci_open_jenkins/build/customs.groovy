b736ea6a888f4bd5fe627cd013212f29647d6c73
mihails-cirganovics <mihails.cirganovics@digital.hmrc.gov.uk>
2018-01-19 10:09:40 +0000

    CDD-1298: build job for customs-notification

--- jobs/ci_open_jenkins/build/customs.groovy
+++ jobs/ci_open_jenkins/build/customs.groovy
@@ -1,38 +1,34 @@
 package ci_open_jenkins.build
 
 import javaposse.jobdsl.dsl.DslFactory
+import javaposse.jobdsl.dsl.Job
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtMicroserviceJobBuilder
 import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.BuildMonitorViewBuilder
 
 def customsInventoryLinkingImports = 'customs-inventory-linking-imports'
 def customsInventoryLinkingExports = 'customs-inventory-linking-exports'
 def customsDeclarations = 'customs-declarations'
+def customsNotification = "customs-notification"
 def customsApiCommon = 'customs-api-common'
 
-new SbtMicroserviceJobBuilder(customsInventoryLinkingImports).
-    withSCoverage().
-    withScalaStyle().
-    build(this as DslFactory)
-
-new SbtMicroserviceJobBuilder(customsInventoryLinkingExports).
+private Job microservice(String serviceName) {
+    new SbtMicroserviceJobBuilder(serviceName).
         withSCoverage().
         withScalaStyle().
         build(this as DslFactory)
+}
 
-new SbtMicroserviceJobBuilder(customsDeclarations).
-        withSCoverage().
-        withScalaStyle().
-        build(this as DslFactory)
-
-new SbtMicroserviceJobBuilder(customsApiCommon).
-    withSCoverage().
-    withScalaStyle().
-    build(this as DslFactory)
+microservice(customsInventoryLinkingImports)
+microservice(customsInventoryLinkingExports)
+microservice(customsDeclarations)
+microservice(customsNotification)
+microservice(customsApiCommon)
 
 new BuildMonitorViewBuilder('CUSTOMS-MONITOR').
     withJobs(
         customsInventoryLinkingImports,
         customsInventoryLinkingExports,
         customsDeclarations,
+        customsNotification,
         customsApiCommon).
     build(this)
