badbff0bff72894bcb18e7d36afd42e74a9638f8
Ramin <ramin.esfan@gmail.com>
2018-03-02 15:41:02 +0000

    TAI-FRONTEND-move-to-open: refactor

--- jobs/ci_open_jenkins/build/tai.groovy
+++ jobs/ci_open_jenkins/build/tai.groovy
@@ -5,14 +5,11 @@ import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.BuildMonitorViewBuilder
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtMicroserviceJobBuilder
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtFrontendJobBuilder
 
-new SbtMicroserviceJobBuilder('tai').
-        withTests("test").
-        withScalaStyle().
-        withSCoverage().
-        build(this as DslFactory)
-
 new BuildMonitorViewBuilder('TAI-MONITOR')
         .withJobs('tai-frontend').build(this)
 
 new SbtFrontendJobBuilder('tai-frontend').
+        withTests("test").
+        withScalaStyle().
+        withSCoverage().
         build(this as DslFactory)
