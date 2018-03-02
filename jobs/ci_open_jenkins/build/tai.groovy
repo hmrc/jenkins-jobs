2d85d9b350e1ec3454d90b0d1d5949813586cc19
Ramin <ramin.esfan@gmail.com>
2018-03-02 15:43:38 +0000

    TAI-FRONTEND-move-to-open

--- jobs/ci_open_jenkins/build/tai.groovy
+++ jobs/ci_open_jenkins/build/tai.groovy
@@ -2,7 +2,6 @@ package ci_open_jenkins.build
 
 import javaposse.jobdsl.dsl.DslFactory
 import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.BuildMonitorViewBuilder
-import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtMicroserviceJobBuilder
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtFrontendJobBuilder
 
 new BuildMonitorViewBuilder('TAI-MONITOR')
