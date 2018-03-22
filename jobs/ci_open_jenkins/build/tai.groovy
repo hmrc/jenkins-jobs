3448b0507e3f8bc2d23af0a203e60c7ece8a59df
yasar.acar <yasar.acar@accenture.com>
2018-03-22 13:54:53 +0000

    Add tai microservice

--- jobs/ci_open_jenkins/build/tai.groovy
+++ jobs/ci_open_jenkins/build/tai.groovy
@@ -3,9 +3,16 @@ package ci_open_jenkins.build
 import javaposse.jobdsl.dsl.DslFactory
 import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.BuildMonitorViewBuilder
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtFrontendJobBuilder
+import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtMicroserviceJobBuilder
 
 new BuildMonitorViewBuilder('TAI-MONITOR')
-        .withJobs('tai-frontend').build(this)
+        .withJobs('tai-frontend', 'tai').build(this)
+
+new SbtMicroserviceJobBuilder('tai').
+        withScalaStyle().
+        withSCoverage().
+        withTests("test").
+        build(this as DslFactory)
 
 new SbtFrontendJobBuilder('tai-frontend').
         withTests("test").
