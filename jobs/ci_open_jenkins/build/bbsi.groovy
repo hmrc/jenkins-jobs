5c5a957d617bca338dc8c5345b1412019dd36280
KrishnaBorra <krishna.borra@digital.hmrc.gov.uk>
2018-04-09 14:32:35 +0100

    BBSI-39 updated bbsi.groovy file to use new repo for BBSI

--- jobs/ci_open_jenkins/build/bbsi.groovy
+++ jobs/ci_open_jenkins/build/bbsi.groovy
@@ -2,18 +2,12 @@ package ci_open_jenkins.build
 
 import javaposse.jobdsl.dsl.DslFactory
 import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.BuildMonitorViewBuilder
-import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtMicroserviceJobBuilder
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtFrontendJobBuilder
 
-new SbtMicroserviceJobBuilder('bbsi').
-        withScalaStyle().
-        withSCoverage().
-        build(this as DslFactory)
-
-new SbtFrontendJobBuilder('bbsi-frontend').
+new SbtFrontendJobBuilder('bbsi-frontend', 'hmrc/bank-and-building-society-interest-frontend', 'master').
         withScalaStyle().
         withSCoverage().
         build(this as DslFactory)
 
 new BuildMonitorViewBuilder('BBSI-MONITOR')
-        .withJobs('bbsi','bbsi-frontend').build(this)
+        .withJobs('bbsi-frontend').build(this)
