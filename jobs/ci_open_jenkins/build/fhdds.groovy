713f61d7b95490fbe0163bd9ac7135df843d50b4
Yuan.ZHANG <yuan.zhang@digital.hmrc.gov.uk>
2018-04-17 14:36:21 +0100

    FHDDS-525:added scoverage for fhdds

--- jobs/ci_open_jenkins/build/fhdds.groovy
+++ jobs/ci_open_jenkins/build/fhdds.groovy
@@ -8,10 +8,12 @@ import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtFrontendJobBuilder
 
 new SbtMicroserviceJobBuilder('fh-registration').
         withTests("test").
+        withSCoverage().
         build(this as DslFactory)
 
 new SbtFrontendJobBuilder('fh-registration-frontend').
         withTests("test").
+        withSCoverage().
         build(this as DslFactory)
 
 new BuildMonitorViewBuilder('FHDDS')
