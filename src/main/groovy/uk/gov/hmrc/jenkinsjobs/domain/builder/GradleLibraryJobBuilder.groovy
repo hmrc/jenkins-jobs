37172fee17d7e354daaf57d12b1807501f26e58c
Edd Grant <edd@eddgrant.com>
2018-04-18 10:25:14 +0100

    PBD-598 - Don't inherit post cleanup for Gradle library builds as it prohibits accessing the HMTL report after the job.

--- src/main/groovy/uk/gov/hmrc/jenkinsjobs/domain/builder/GradleLibraryJobBuilder.groovy
+++ src/main/groovy/uk/gov/hmrc/jenkinsjobs/domain/builder/GradleLibraryJobBuilder.groovy
@@ -7,7 +7,9 @@ import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.JobBuilder
 
 import static uk.gov.hmrc.jenkinsjobbuilders.domain.publisher.HtmlReportsPublisher.htmlReportsPublisher
 import static uk.gov.hmrc.jenkinsjobbuilders.domain.publisher.JUnitReportsPublisher.jUnitReportsPublisher
+import static uk.gov.hmrc.jenkinsjobbuilders.domain.trigger.PollTrigger.pollTrigger
 import static uk.gov.hmrc.jenkinsjobbuilders.domain.wrapper.AbsoluteTimeoutWrapper.timeoutWrapper
+import static uk.gov.hmrc.jenkinsjobs.domain.scm.HmrcGitHubComScm.hmrcGitHubComScm
 import static uk.gov.hmrc.jenkinsjobs.domain.step.Steps.gradleCleanTestBuild
 
 final class GradleLibraryJobBuilder implements Builder {
@@ -17,7 +19,10 @@ final class GradleLibraryJobBuilder implements Builder {
     private int timeout = 10
 
     GradleLibraryJobBuilder(String name, String repository = name) {
-        this.jobBuilder = JobBuilders.jobBuilder(name, repository)
+        this.jobBuilder = JobBuilders.jobBuilder(name)
+            .withScm(hmrcGitHubComScm(repository, 'master'))
+            .withTriggers(pollTrigger("H/5 * * * *"))
+            .withLabel('single-executor')
             .withSteps(gradleCleanTestBuild())
             .withPublishers(jUnitReportsPublisher("build/test-results/test/*.xml"),
                             htmlReportsPublisher(["build/reports/tests/test/" : "HTML Test report"]))
