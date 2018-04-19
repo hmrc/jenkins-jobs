0e0f9d54957aefb6f9580582d39240c8fbcefee4
Edd Grant <edd@eddgrant.com>
2018-04-19 13:50:56 +0100

    PBD-598 - Use the correct type of view for the build-and-deploy view.

--- jobs/ci_open_jenkins/build/build-and-deploy.groovy
+++ jobs/ci_open_jenkins/build/build-and-deploy.groovy
@@ -1,4 +1,4 @@
-import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.ListViewBuilder
+import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.BuildMonitorViewBuilder
 import uk.gov.hmrc.jenkinsjobs.domain.builder.GradleLibraryJobBuilder
 import uk.gov.hmrc.jenkinsjobs.domain.builder.GradleLibraryReleaseJobBuilder
 
@@ -8,4 +8,6 @@ new GradleLibraryJobBuilder('jenkins-job-builders').
 new GradleLibraryReleaseJobBuilder('jenkins-job-builders').
     build(this)
 
-new ListViewBuilder("build-and-deploy").withJobsRegex('^jenkins\\-job\\-builders.*$')
+new BuildMonitorViewBuilder("BUILD-AND-DEPLOY")
+    .withJobs("jenkins-job-builders", "jenkins-job-builders-release")
+    .build(this)
