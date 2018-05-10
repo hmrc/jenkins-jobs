c9f15b298d63e95b24c5b6d425228c3aa112a5cd
Michael Kitchen <kitcheonon@hotmail.com>
2018-05-10 13:53:04 +0100

    GCI-47 amended jenkins job

--- jobs/ci_open_jenkins/build/rtii.groovy
+++ jobs/ci_open_jenkins/build/rtii.groovy
@@ -4,7 +4,5 @@ import javaposse.jobdsl.dsl.DslFactory
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtMicroserviceJobBuilder
 
 new SbtMicroserviceJobBuilder('retrieve-citizen-income-stub').
-        withScalaStyle().
-        withSCoverage().
         withTests("test").
         build(this as DslFactory)
