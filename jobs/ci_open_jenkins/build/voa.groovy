57d470c801e92d327024b8d3d12b3e786dbc8dde
Peter Hazell <peter.hazell@digital.hmrc.gov.uk>
2018-06-11 16:04:14 +0100

    New voa build monitor

--- jobs/ci_open_jenkins/build/voa.groovy
+++ jobs/ci_open_jenkins/build/voa.groovy
@@ -1,6 +1,7 @@
 package ci_open_jenkins.build
 
 import javaposse.jobdsl.dsl.DslFactory
+import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.BuildMonitorViewBuilder
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtLibraryJobBuilder
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtFrontendJobBuilder
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtMicroserviceJobBuilder
@@ -28,3 +29,9 @@ new SbtFrontendJobBuilder('voa-property-linking-frontend-hotfix', 'voa-property-
 new SbtMicroserviceJobBuilder('business-rates-authorisation')
         .withSCoverage()
         .build(this as DslFactory)
+
+new BuildMonitorViewBuilder('CCA-DFE-BUILD-MONITOR-PUBLIC')
+        .withJobs('voa-property-linking-frontend',
+        'voa-property-linking',
+        'business-rates-authorisation')
+        .build(this)
