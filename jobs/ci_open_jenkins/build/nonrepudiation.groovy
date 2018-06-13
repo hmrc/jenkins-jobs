0f095e33ebcc6eab43ba0b4143aab1f7b4a15d31
Craig Duncan <craig.duncan@digital.hmrc.gov.uk>
2018-06-13 15:48:20 +0100

    [NONPR-620][CD] Added monitor for build jobs

--- jobs/ci_open_jenkins/build/nonrepudiation.groovy
+++ jobs/ci_open_jenkins/build/nonrepudiation.groovy
@@ -1,6 +1,7 @@
 package ci_open_jenkins.build
 
 import javaposse.jobdsl.dsl.DslFactory
+import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.BuildMonitorViewBuilder
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtFrontendJobBuilder
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtMicroserviceJobBuilder
 
@@ -13,3 +14,6 @@ new SbtMicroserviceJobBuilder('nrs-retrieval')
         .withScalaStyle()
         .withSCoverage()
         .build(this as DslFactory)
+
+new BuildMonitorViewBuilder('NON-REPUDIATION-BUILD-MONITOR')
+        .withJobs('nrs-retrieval-frontend', 'nrs-retrieval').build(this)
