29943658a1c17fffcb2473860bbecfb56c5327a5
Christopher Townson <iteratoruk@gmail.com>
2018-07-02 15:10:53 +0100

    Update job name reference for CDS import decs

--- jobs/ci_open_jenkins/build/customs-imports.groovy
+++ jobs/ci_open_jenkins/build/customs-imports.groovy
@@ -4,9 +4,9 @@ import javaposse.jobdsl.dsl.DslFactory
 import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.BuildMonitorViewBuilder
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtFrontendJobBuilder
 
-new SbtFrontendJobBuilder('customs-declare-imports').
+new SbtFrontendJobBuilder('customs-declare-imports-frontend').
         build(this as DslFactory)
 
 new BuildMonitorViewBuilder('CDS-IMPORT-DECLARATIONS-MONITOR')
-        .withJobs('customs-declare-imports')
+        .withJobs('customs-declare-imports-frontend')
         .build(this)
