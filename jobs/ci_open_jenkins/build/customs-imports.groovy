d19285cc331be00617bc621bfa503ca56e7bd192
Christopher Townson <iteratoruk@gmail.com>
2018-07-02 14:19:12 +0100

    Add jenkins jobs for customs declarations imports

---
+++ jobs/ci_open_jenkins/build/customs-imports.groovy
@@ -0,0 +1,12 @@
+package ci_open_jenkins.build
+
+import javaposse.jobdsl.dsl.DslFactory
+import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.BuildMonitorViewBuilder
+import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtFrontendJobBuilder
+
+new SbtFrontendJobBuilder('customs-declare-imports').
+        build(this as DslFactory)
+
+new BuildMonitorViewBuilder('CDS-IMPORT-DECLARATIONS-MONITOR')
+        .withJobs('customs-declare-imports')
+        .build(this)
