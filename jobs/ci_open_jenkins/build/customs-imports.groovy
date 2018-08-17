b8f028d43f322b2fd75289e0279d389edd29b5b7
raghu1978 <raghu.adimulam1@digital.hmrc.gov.uk>
2018-08-17 12:59:23 +0100

    [CDSID] - added customs-declarations-stub

--- jobs/ci_open_jenkins/build/customs-imports.groovy
+++ jobs/ci_open_jenkins/build/customs-imports.groovy
@@ -3,8 +3,13 @@ package ci_open_jenkins.build
 import javaposse.jobdsl.dsl.DslFactory
 import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.BuildMonitorViewBuilder
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtFrontendJobBuilder
+import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtMicroserviceJobBuilder
 
 new SbtFrontendJobBuilder('customs-declare-imports-frontend').
+        build(this as DslFactory)
+
+new SbtMicroserviceJobBuilder('customs-declarations-stub').
+        withTests("test").
         build(this as DslFactory)
 
 new BuildMonitorViewBuilder('CDS-IMPORT-DECLARATIONS-MONITOR')
