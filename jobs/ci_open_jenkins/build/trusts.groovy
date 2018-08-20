03c90ac07786deef319d96f7b3d17ee451d9ab80
damien.cooke <damien.cooke@digital.hmrc.gov.uk>
2018-08-20 15:42:43 +0100

    TRUSTS-1640 | Added jenkins job for trusts frontend

--- jobs/ci_open_jenkins/build/trusts.groovy
+++ jobs/ci_open_jenkins/build/trusts.groovy
@@ -3,11 +3,19 @@ package ci_open_jenkins.build
 import javaposse.jobdsl.dsl.DslFactory
 import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.BuildMonitorViewBuilder
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtMicroserviceJobBuilder
+import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtFrontendJobBuilder
 
 new SbtMicroserviceJobBuilder('trust-registration-api').
         withScalaStyle().
         withSCoverage().
         build(this as DslFactory)
 
+new SbtFrontendJobBuilder('trusts-frontend').
+        withScalaStyle().
+        withSCoverage().
+        withXvfb().
+        build(this as DslFactory)
+
 new BuildMonitorViewBuilder('TRUST-MONITOR')
-        .withJobs('trust-registration-api').build(this)
+        .withJobs('trust-registration-api','trusts-frontend')
+        .build(this)
