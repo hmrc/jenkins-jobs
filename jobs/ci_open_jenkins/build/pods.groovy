8f940972aa9e964c5f8913255eb41a07d16611b2
Andrew Arkless <AndrewArkless@users.noreply.github.com>
2018-08-21 13:52:57 +0100

    PODS-1454 removing Scheme, Scheme Fe and PSA FE

--- jobs/ci_open_jenkins/build/pods.groovy
+++ jobs/ci_open_jenkins/build/pods.groovy
@@ -1,24 +1,9 @@
-package ci_open_jenkins.build
+g package ci_open_jenkins.build
 
 import javaposse.jobdsl.dsl.DslFactory
 import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.BuildMonitorViewBuilder
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtMicroserviceJobBuilder
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtFrontendJobBuilder
-
-new SbtFrontendJobBuilder('pensions-scheme-frontend').
-        withScalaStyle().
-        withSCoverage().
-        build(this as DslFactory)
-
-new SbtFrontendJobBuilder('pension-administrator-frontend').
-        withScalaStyle().
-        withSCoverage().
-        build(this as DslFactory)
-
-new SbtMicroserviceJobBuilder('pensions-scheme').
-        withScalaStyle().
-        withSCoverage().
-        build(this as DslFactory)
 
 new BuildMonitorViewBuilder('PODS-MONITOR')
         .withJobs('pensions-scheme-frontend',
