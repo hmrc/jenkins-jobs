af40c00fe69db37b642b3a65920d6bfcce656f51
Christopher Townson <iteratoruk@gmail.com>
2018-08-28 18:19:53 +0100

    Add wco-dec library build job

--- jobs/ci_open_jenkins/build/customs-imports.groovy
+++ jobs/ci_open_jenkins/build/customs-imports.groovy
@@ -3,14 +3,18 @@ package ci_open_jenkins.build
 import javaposse.jobdsl.dsl.DslFactory
 import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.BuildMonitorViewBuilder
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtFrontendJobBuilder
+import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtLibraryJobBuilder
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtMicroserviceJobBuilder
 
-new SbtFrontendJobBuilder('customs-declare-imports-frontend').
+new SbtFrontendJobBuilder("customs-declare-imports-frontend").
         build(this as DslFactory)
 
-new SbtMicroserviceJobBuilder('customs-declarations-stub').
+new SbtMicroserviceJobBuilder("customs-declarations-stub").
         build(this as DslFactory)
 
-new BuildMonitorViewBuilder('CDS-IMPORT-DECLARATIONS-MONITOR')
-        .withJobs('customs-declare-imports-frontend', 'customs-declarations-stub')
+new SbtLibraryJobBuilder("wco-dec").
+        build(this as DslFactory)
+
+new BuildMonitorViewBuilder("CDS-IMPORT-DECLARATIONS-MONITOR")
+        .withJobs("customs-declare-imports-frontend", "customs-declarations-stub", "wco-dec")
         .build(this)
