3b1426f0d0706a9b4ab297af7cb64459c8c65e6c
cavster <colm.m.cavanagh@gmail.com>
2018-09-06 13:33:43 +0100

    [CC]Add in scala style and scoverage to self service time to pay frontend

--- jobs/ci_open_jenkins/build/ssttp.groovy
+++ jobs/ci_open_jenkins/build/ssttp.groovy
@@ -1,26 +1,21 @@
 import javaposse.jobdsl.dsl.DslFactory
 import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.BuildMonitorViewBuilder
-import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtFrontendJobBuilder
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtMicroserviceJobBuilder
 
-def frontends = ["self-service-time-to-pay-frontend"]
+def services = [
+        "self-service-time-to-pay-frontend",
+        "ia-frontend",
+        "self-service-time-to-pay",
+        "self-service-time-to-pay-des-stub",
+        "self-service-time-to-pay-des-stub-scala",
+        "time-to-pay-arrangement",
+        "time-to-pay-calculator",
+        "time-to-pay-eligibility",
+        "time-to-pay-taxpayer",
+        "ia",
+        "sa-stub"]
 
-def services = ["self-service-time-to-pay",
-                    "self-service-time-to-pay-des-stub",
-                    "self-service-time-to-pay-des-stub-scala",
-                    "time-to-pay-arrangement",
-                    "time-to-pay-calculator",
-                    "time-to-pay-eligibility",
-                    "time-to-pay-taxpayer",
-                    "ia-frontend",
-                    "ia",
-                    "sa-stub"]
-
-def allServices = frontends + services
-
-frontends.each {
-    new SbtFrontendJobBuilder(it).build(this as DslFactory)
-}
+def allServices = services
 
 services.each {
     new SbtMicroserviceJobBuilder(it)
