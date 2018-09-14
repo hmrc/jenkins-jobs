5012681fcc1d6196424795eeccef3c2f510a91bb
robert-hawkes <robert.hawkes@digital.hmrc.gov.uk>
2018-09-14 14:10:50 +0100

    Remove income-tax-subscription

--- jobs/ci_open_jenkins/build/sabr.groovy
+++ jobs/ci_open_jenkins/build/sabr.groovy
@@ -5,11 +5,6 @@ import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.BuildMonitorViewBuilder
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtMicroserviceJobBuilder
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtFrontendJobBuilder
 
-new SbtMicroserviceJobBuilder('income-tax-subscription').
-        withScalaStyle().
-        withSCoverage().
-        build(this as DslFactory)
-
 new SbtMicroserviceJobBuilder('income-tax-subscription-store').
         withScalaStyle().
         withSCoverage().
@@ -37,5 +32,5 @@ new SbtFrontendJobBuilder('vat-sign-up-frontend').
         build(this as DslFactory)
 
 new BuildMonitorViewBuilder('SABR-MONITOR').withJobs(
-        'income-tax-subscription', 'income-tax-subscription-frontend', 'income-tax-subscription-store','vat-sign-up-frontend', 'vat-sign-up'
+        'income-tax-subscription-frontend', 'income-tax-subscription-store', 'vat-sign-up-frontend', 'vat-sign-up'
 ).build(this)
