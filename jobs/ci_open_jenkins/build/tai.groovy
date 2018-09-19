b4b183708ff9eb0f3eeba2da08edd802d4bea190
Nicole Avison <nicole.avison@digital.hmrc.gov.uk>
2018-09-19 14:45:14 +0100

    remiving tai backend job

--- jobs/ci_open_jenkins/build/tai.groovy
+++ jobs/ci_open_jenkins/build/tai.groovy
@@ -7,9 +7,3 @@ import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtMicroserviceJobBuilder
 
 new BuildMonitorViewBuilder('TAI-MONITOR')
         .withJobs('tai-frontend', 'tai').build(this)
-
-new SbtMicroserviceJobBuilder('tai').
-        withScalaStyle().
-        withSCoverage().
-        withTests("test").
-        build(this as DslFactory)
