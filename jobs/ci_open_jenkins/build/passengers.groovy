d3648072e6e9ab3d07dae82bdfe666d0a60ef4fc
thomas curry <thomasjcurry@hotmail.com>
2018-09-20 12:52:10 +0100

    Remove passengers.groovy

--- jobs/ci_open_jenkins/build/passengers.groovy
+++
@@ -1,9 +0,0 @@
-package ci_open_jenkins.build
-
-import javaposse.jobdsl.dsl.DslFactory
-import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.BuildMonitorViewBuilder
-import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtFrontendJobBuilder
-import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtMicroserviceJobBuilder
-
-new BuildMonitorViewBuilder('PASSENGERS-MONITOR')
-        .withJobs('currency-conversion').build(this)
