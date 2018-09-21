6dc6180c3d9d027f5e2f4f4f7cef0a583d420a4b
Peter Hazell <peter.hazell@digital.hmrc.gov.uk>
2018-09-21 16:07:29 +0100

    VTCCA-2150 Remove old voa dfe open jobs

--- jobs/ci_open_jenkins/build/voa.groovy
+++ jobs/ci_open_jenkins/build/voa.groovy
@@ -1,37 +1,11 @@
 package ci_open_jenkins.build
 
 import javaposse.jobdsl.dsl.DslFactory
-import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.BuildMonitorViewBuilder
-import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtLibraryJobBuilder
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtFrontendJobBuilder
-import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtMicroserviceJobBuilder
+import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtLibraryJobBuilder
 
 new SbtLibraryJobBuilder('play-conditional-form-mapping').
         build(this as DslFactory)
 
 new SbtFrontendJobBuilder('for-frontend')
         .build(this as DslFactory)
-
-new SbtFrontendJobBuilder('voa-property-linking-frontend')
-        .withSCoverage()
-        .build(this as DslFactory)
-
-new SbtFrontendJobBuilder('voa-property-linking')
-        .withSCoverage()
-        .build(this as DslFactory)
-
-new SbtFrontendJobBuilder('voa-property-linking-hotfix', 'voa-property-linking', 'vpl-hotfix')
-        .build(this as DslFactory)
-
-new SbtFrontendJobBuilder('voa-property-linking-frontend-hotfix', 'voa-property-linking-frontend', 'vplf-hotfix')
-        .build(this as DslFactory)
-
-new SbtMicroserviceJobBuilder('business-rates-authorisation')
-        .withSCoverage()
-        .build(this as DslFactory)
-
-new BuildMonitorViewBuilder('CCA-DFE-BUILD-MONITOR-PUBLIC')
-        .withJobs('voa-property-linking-frontend',
-        'voa-property-linking',
-        'business-rates-authorisation')
-        .build(this)
