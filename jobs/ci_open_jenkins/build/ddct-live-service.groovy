7c35e2a9f6ade9a2ab405e42e5d3f14b5b04fe86
Paul Anderson <paul.anderson@digital.hmrc.gov.uk>
2018-09-24 17:01:54 +0100

    DL-1247 removing Childcare related config for build migration

--- jobs/ci_open_jenkins/build/ddct-live-service.groovy
+++ jobs/ci_open_jenkins/build/ddct-live-service.groovy
@@ -24,30 +24,6 @@ new SbtMicroserviceJobBuilder('pla-dynamic-stub')
         .withSCoverage()
         .build(this as DslFactory)
 
-new SbtFrontendJobBuilder('childcare-calculator-frontend').
-        withScalaStyle().
-        withSCoverage().
-        withTests("test").
-        build(this as DslFactory)
-
-new SbtMicroserviceJobBuilder('cc-calculator').
-        withScalaStyle().
-        withTests("test").
-        withSCoverage().
-        build(this as DslFactory)
-
-new SbtMicroserviceJobBuilder('cc-eligibility').
-        withScalaStyle().
-        withSCoverage().
-        withTests("test").
-        build(this as DslFactory)
-
-new SbtMicroserviceJobBuilder('cc-email-capture').
-        withScalaStyle().
-        withSCoverage().
-        withTests("test").
-        build(this as DslFactory)
-
 new SbtMicroserviceJobBuilder('residence-nil-rate-band-calculator').
         withTests('test').
         withScalaStyle().
@@ -81,10 +57,6 @@ new BuildMonitorViewBuilder('DDCT-LIVE-SERVICES-MONITOR')
         'pensions-lifetime-allowance',
         'pensions-lifetime-allowance-frontend',
         'pla-dynamic-stub',
-        'childcare-calculator-frontend',
-        'cc-calculator',
-        'cc-eligibility',
-        'cc-email-capture',
         'residence-nil-rate-band-calculator',
         'residence-nil-rate-band-calculator-frontend',
         'off-payroll-frontend',
