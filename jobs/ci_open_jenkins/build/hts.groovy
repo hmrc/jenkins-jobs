e38d58e4a3733839f26e7d1bd3240d2dffc1b05b
MarcusQA <marcus.cumming@gmail.com>
2018-09-27 13:29:45 +0100

    HTS: Deleting back end job as part of migration

--- jobs/ci_open_jenkins/build/hts.groovy
+++ jobs/ci_open_jenkins/build/hts.groovy
@@ -5,11 +5,11 @@ import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.BuildMonitorViewBuilder
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtFrontendJobBuilder
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtMicroserviceJobBuilder
 
-new SbtMicroserviceJobBuilder('help-to-save').
-        withScalaStyle().
-        withSCoverage().
-        withTests("test").
-        build(this as DslFactory)
+//new SbtMicroserviceJobBuilder('help-to-save').
+//        withScalaStyle().
+//        withSCoverage().
+//        withTests("test").
+//        build(this as DslFactory)
 
 new SbtFrontendJobBuilder('help-to-save-frontend').
         withExtendedTimeout().
