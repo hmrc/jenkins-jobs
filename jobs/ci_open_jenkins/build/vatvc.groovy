5d6126cce6b99fcb4cd042fb512578542508508a
davidmthornton <david.thornton@digital.hmrc.gov.uk>
2018-09-28 10:54:31 +0100

    BTAT-3990: Removed vat-correspondence-details-frontend for new build

--- jobs/ci_open_jenkins/build/vatvc.groovy
+++ jobs/ci_open_jenkins/build/vatvc.groovy
@@ -5,19 +5,11 @@ import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.BuildMonitorViewBuilder
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtFrontendJobBuilder
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtMicroserviceJobBuilder
 
-new SbtMicroserviceJobBuilder('vat-correspondence-details-frontend').
-        withScalaStyle().
-        withSCoverage().
-        build(this as DslFactory)
-
 new SbtMicroserviceJobBuilder('vat-obligations-dynamic-stub').
         withScalaStyle().
         withSCoverage().
         build(this as DslFactory)
 
 new BuildMonitorViewBuilder('VATVC-MONITOR').withJobs(
-        'vat-correspondence-details-frontend',
-        'vat-obligations-dynamic-stub',
-        'financial-transactions',
-        'financial-transactions-dynamic-stub'
+        'vat-obligations-dynamic-stub'
 ).build(this)
