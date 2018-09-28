fb20cfe8d33eb41c2dfe8cfbb51f75b5e74b11ab
Gary Hull <gary.hull@digital.hmrc.gov.uk>
2018-09-28 13:49:12 +0100

    removed financial transactions from financial-transactions.groovy

--- jobs/ci_open_jenkins/build/financial-transactions.groovy
+++ jobs/ci_open_jenkins/build/financial-transactions.groovy
@@ -4,11 +4,6 @@ import javaposse.jobdsl.dsl.DslFactory
 import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.BuildMonitorViewBuilder
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtMicroserviceJobBuilder
 
-new SbtMicroserviceJobBuilder('financial-transactions')
-        .withScalaStyle()
-        .withSCoverage()
-		    .build(this as DslFactory)
-
 new SbtMicroserviceJobBuilder('financial-transactions-dynamic-stub').
 		withScalaStyle().
 		withSCoverage().
