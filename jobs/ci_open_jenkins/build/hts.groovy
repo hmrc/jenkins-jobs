a81ed3b96db4839bcbaf0332584cdd5306ec2445
Suresh Mandalapu <suresh.mandalapu@digital.hmrc.gov.uk>
2018-10-01 14:58:30 +0530

    HTS-1336: Migrating Help to save to new build config

--- jobs/ci_open_jenkins/build/hts.groovy
+++ jobs/ci_open_jenkins/build/hts.groovy
@@ -1,53 +1,6 @@
 package ci_open_jenkins.build
 
-import javaposse.jobdsl.dsl.DslFactory
 import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.BuildMonitorViewBuilder
-import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtFrontendJobBuilder
-import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtMicroserviceJobBuilder
-
-//new SbtMicroserviceJobBuilder('help-to-save').
-//        withScalaStyle().
-//        withSCoverage().
-//        withTests("test").
-//        build(this as DslFactory)
-
-new SbtFrontendJobBuilder('help-to-save-frontend').
-        withExtendedTimeout().
-        withScalaStyle().
-        withSCoverage().
-        withTests("test").
-        build(this as DslFactory)
-
-new SbtMicroserviceJobBuilder('help-to-save-stub').
-        withScalaStyle().
-        withSCoverage().
-        withTests("test").
-        build(this as DslFactory)
-
-new SbtMicroserviceJobBuilder('help-to-save-api').
-        withScalaStyle().
-        withSCoverage().
-        withTests("test").
-        build(this as DslFactory)
-
-new SbtMicroserviceJobBuilder('help-to-save-proxy').
-        withScalaStyle().
-        withSCoverage().
-        withTests("test").
-        build(this as DslFactory)
-
-new SbtFrontendJobBuilder('help-to-save-stride-frontend').
-        withExtendedTimeout().
-        withScalaStyle().
-        withSCoverage().
-        withTests("test").
-        build(this as DslFactory)
-
-new SbtFrontendJobBuilder('help-to-save-test-admin-frontend').
-        withExtendedTimeout().
-        withSCoverage().
-        withTests("test").
-        build(this as DslFactory)
 
 new BuildMonitorViewBuilder('HELP-TO-SAVE-MONITOR')
         .withJobs('help-to-save','help-to-save-frontend', 'help-to-save-stub', 'help-to-save-proxy', 'help-to-save-stride-frontend', 'help-to-save-api', 'help-to-save-test-admin-frontend').build(this)
