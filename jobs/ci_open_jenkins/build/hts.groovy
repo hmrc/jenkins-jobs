d9d5226c781671737bde74bd41213dcd906204bc
Suresh Mandalapu <suresh.mandalapu@digital.hmrc.gov.uk>
2018-10-01 17:28:53 +0530

    Delete hts.groovy

--- jobs/ci_open_jenkins/build/hts.groovy
+++
@@ -1,7 +0,0 @@
-package ci_open_jenkins.build
-
-import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.BuildMonitorViewBuilder
-
-new BuildMonitorViewBuilder('HELP-TO-SAVE-MONITOR')
-        .withJobs('help-to-save','help-to-save-frontend', 'help-to-save-stub', 'help-to-save-proxy', 'help-to-save-stride-frontend', 'help-to-save-api', 'help-to-save-test-admin-frontend').build(this)
-
