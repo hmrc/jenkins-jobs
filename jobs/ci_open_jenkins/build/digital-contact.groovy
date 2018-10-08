60e142c20e7647c06a802ca49d58979cfabff57e
Jakub Chrobasik <jakub.chrobasik@gmail.com>
2018-10-08 17:22:33 +0100

    PLATOPS-1566 Removed jobs for removed work-item-repo-26 and metrix-26

--- jobs/ci_open_jenkins/build/digital-contact.groovy
+++ jobs/ci_open_jenkins/build/digital-contact.groovy
@@ -1,20 +1,7 @@
 package ci_open_jenkins.build
 
-import javaposse.jobdsl.dsl.DslFactory
 import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.BuildMonitorViewBuilder
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtLibraryJobBuilder
-
-        
-
-
-new SbtLibraryJobBuilder('work-item-repo-26').
-        build(this)
-
-
-new SbtLibraryJobBuilder('metrix-26').
-        build(this)
-
-
 
 new SbtLibraryJobBuilder('batch-updater').
         build(this)
