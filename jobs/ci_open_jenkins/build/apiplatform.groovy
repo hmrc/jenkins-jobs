3f7fee3031f6459914753062f5530976f10e64b4
Andy Spaven <andy.spaven@digital.hmrc.gov.uk>
2018-10-09 08:50:01 +0100

    API-3441 - Remove api-scope from old build tooling

--- jobs/ci_open_jenkins/build/apiplatform.groovy
+++ jobs/ci_open_jenkins/build/apiplatform.groovy
@@ -14,10 +14,6 @@ new SbtFrontendJobBuilder('api-service-approval-frontend').
 new SbtFrontendJobBuilder('api-sandbox-holding-page-frontend').
         build(this as DslFactory)
 
-new SbtMicroserviceJobBuilder('api-scope').
-        withSCoverage().
-        build(this as DslFactory)
-
 new SbtMicroserviceJobBuilder('api-stop-autodeploy-test').
         withTests("test").
         withSCoverage().
