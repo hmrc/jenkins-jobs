60ba48010985022bc4bbbbbd57fd2e8c608b079e
Ian Stainer <ian.stainer@valtech.co.uk>
2018-10-12 12:10:03 +0100

    Disabled fset-faststream and fset-faststream-frontend

--- jobs/ci_open_jenkins/build/csr.groovy
+++ jobs/ci_open_jenkins/build/csr.groovy
@@ -5,14 +5,14 @@ import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.BuildMonitorViewBuilder
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtFrontendJobBuilder
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtMicroserviceJobBuilder
 
-
+/*
 new SbtMicroserviceJobBuilder('fset-faststream').
     withExtendedTimeout().
     build(this as DslFactory)
 
 new SbtFrontendJobBuilder('fset-faststream-frontend').
     build(this as DslFactory)
-/*
+
 new SbtFrontendJobBuilder('fset-video-interview-gateway').
     build(this as DslFactory)
 
