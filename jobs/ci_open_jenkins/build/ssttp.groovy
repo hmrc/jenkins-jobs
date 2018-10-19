b2e4fd2acc0ea5465925f124ca748e560d9c6240
Colm Cavanagh <colm.m.cavanagh@gmail.com>
2018-10-19 14:28:46 +0100

    [CC]Removed ia and ia-frontend from jenkins jobs (#930)

--- jobs/ci_open_jenkins/build/ssttp.groovy
+++ jobs/ci_open_jenkins/build/ssttp.groovy
@@ -3,10 +3,8 @@ import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.BuildMonitorViewBuilder
 import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtMicroserviceJobBuilder
 
 def services = [
-        "ia-frontend",
        // "self-service-time-to-pay",
         "time-to-pay-arrangement",
-        "ia",
         "sa-stub"]
 
 def allServices = services
