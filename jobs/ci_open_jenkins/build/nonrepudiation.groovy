7c33fec5f729af0a909633358a4ca7da07e4c957
Chris Rudd <chris.rudd@digital.hmrc.gov.uk>
2018-11-05 09:41:59 +0000

    Remove unusued components.

--- jobs/ci_open_jenkins/build/nonrepudiation.groovy
+++ jobs/ci_open_jenkins/build/nonrepudiation.groovy
@@ -1,6 +1,1 @@
 package ci_open_jenkins.build
-
-import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.BuildMonitorViewBuilder
-
-new BuildMonitorViewBuilder('NON-REPUDIATION-BUILD-MONITOR')
-        .withJobs('nrs-retrieval-frontend', 'nrs-retrieval').build(this)
