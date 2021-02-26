79b37ceb3b60d7d2b07be948efd4daa43e8846a5
Mark Collin <1205116+Ardesco@users.noreply.github.com>
2021-02-26 10:27:15 +0000

    #TAV-179 Remove old deprecated config from txm-north.groovy

--- jobs/ci_open_jenkins/build/txm-north.groovy
+++ jobs/ci_open_jenkins/build/txm-north.groovy
@@ -8,22 +8,9 @@ import uk.gov.hmrc.jenkinsjobs.domain.builder.SbtFrontendJobBuilder
 
 new SbtLibraryJobBuilder('logging').build(this as DslFactory)
 
-new SbtLibraryJobBuilder('address-reputation-store').build(this as DslFactory)
-
 new SbtLibraryJobBuilder('play-random-json-filter').build(this as DslFactory)
 
 new SbtLibraryJobBuilder('txm-events').build(this as DslFactory)
-
-new SbtMicroserviceJobBuilder('address-lookup').
-        withScalaStyle().
-        withSCoverage().
-        build(this as DslFactory)
-
-new SbtFrontendJobBuilder('address-lookup-frontend').
-        build(this as DslFactory)
-
-new SbtFrontendJobBuilder('address-lookup-frontend-stub').
-        build(this as DslFactory)
 
 new SbtFrontendJobBuilder('kafka-amqp-sink').
         withScalaStyle().
@@ -32,9 +19,5 @@ new SbtFrontendJobBuilder('kafka-amqp-sink').
 
 new BuildMonitorViewBuilder('TXM-NORTH-MONITOR')
         .withJobs('logging',
-                  'address-reputation-store',
-                  'address-lookup',
-                  'address-lookup-frontend',
-                  'address-lookup-frontend-stub',
                   'play-random-json-filter',
                   'txm-events').build(this)
